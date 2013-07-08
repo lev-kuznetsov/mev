303287722797bbea82fc7f999e6d8b4fe4bda986
Jalil F <jrab227@gmail.com>
2013-07-08 16:19:56 -0400

    Modified controllerSpec and moved test to End2End Scenario

--- src/main/webapp/META-INF/resources/mev/html/test/unit/controllersSpec.js
+++ src/main/webapp/META-INF/resources/mev/html/test/unit/controllersSpec.js
@@ -10,40 +10,15 @@ describe('myApp controllers', function(){
 	
     var scope, ctrl, $httpBackend;
 	  
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-      
-      //HTTP Mock Setup
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('heatmap/test-file').
-          respond([
-             {data: [1, 2, 3, 4]},
-             {rows: 2},
-             {columns: 2}
-          ]);
-      
-      
+    beforeEach(inject(function($rootScope, $controller) {
+		
       scope = $rootScope.$new(),
       ctrl = $controller('HeatmapCtrl', {$scope: scope});
             
     }));
     
-    describe('data downloading', function() {
-		
-      beforeEach(function() {
-		  
-		expect(scope.matrixLocation).toBeUndefined();
-		browser().navigateTo('../../app/index.html#/heatmap/test-file');
-	  
-	  });
-	  
-      it('should update matrixLocation based on url :matrixLocation parameter', function() {
-        expect(binding('matrixLocation')).toBe('test-file');
-      });
-      
-      it('should have loaded matrix data from http request', function() {
-        
-      });
-      
+    it('should have loaded matrix data from http request', function() {
+      expect(scope.matrixLocation).toBeUndefined();
     });
     
     it('should generate default cell parameters that match default size', function() {
