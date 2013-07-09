25f30793276154ccd87ba6f8173ff4dc25824547
correiak <correia.kev@gmail.com>
2013-07-09 13:01:35 -0400

    Testing committ, contians file upoad stuff

--- src/main/webapp/META-INF/resources/mev/html/app/js/controllers.js
+++ src/main/webapp/META-INF/resources/mev/html/app/js/controllers.js
@@ -9,3 +9,40 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }]);
+  .controller('controllerUpload', [function($scope){
+    //Function to upload files
+    $scope.sendFile = function(){
+      //Assign upload html elements to javascript variables. Used to track file upload progress.
+      var bar = $("#bar");
+      var percent = $("#percent");
+      var output = $("#output");
+      $(document).ready(function() 
+      {
+        var options = {
+          //HTML element to receive server response
+          target: "#output",
+          //Tracks file upload progress
+          uploadProgress: function(event, position, total, percentComplete){
+            var percentVal = percentComplete + '%';
+            bar.width(percentVal); //Grow the bar as file progress continues. Expressed as a percent.
+            percent.html(percentVal); //Update the text that indicates what percent the upload progress is at. Percent expressed as text.
+            output.html("Uploading..."); //Indicates to user that file upload is in progress.
+          },
+          success: function(){
+            //HTML status upon successful file upload.
+            var percentVal = "100%";
+            bar.width(percentVal);
+            percent.html(percentVal);
+          }
+        };
+        //Prep form and pass options to jquery plugin to get ready for submission.  
+        $('#myform').ajaxForm(options);
+      });
+      //Submit the form.  
+      $('myform').submit(function()
+      {
+        $(this).ajaxSubmit();
+        return false; //This command is used to prevent any page navigation during form submission to ensure ajax experience.
+      });	
+    };
+  };
