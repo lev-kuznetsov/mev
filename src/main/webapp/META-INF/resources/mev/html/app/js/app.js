f776f76573f173f7476295a55379fd7e583d4a2b
Jalil F <jrab227@gmail.com>
2013-07-11 12:33:32 -0400

    hold

--- src/main/webapp/META-INF/resources/mev/html/app/js/app.js
+++ src/main/webapp/META-INF/resources/mev/html/app/js/app.js
@@ -4,7 +4,14 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/heatmap/:matrixLocation', {templateUrl: 'partials/heatmap-view.html', controller: 'HeatmapCtrl'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+	$routeProvider.when('/home', {
+		templateUrl: 'partials/home-view.html', 
+		controller: 'HomeCtrl'});
+	$routeProvider.when('/about', {
+		templateUrl: 'partials/about-view.html', 
+		controller: 'HeatmapCtrl'});
+    $routeProvider.when('/heatmap/:matrixLocation', {
+		templateUrl: 'partials/heatmap-view.html', 
+		controller: 'HeatmapCtrl'});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
