bb4329c734a18ccb4cc75c6b6a9d48544dbd643d
Jalil F <jrab227@gmail.com>
2013-07-23 17:29:40 -0400

    Actual Update of Effeckt stylings

--- src/main/webapp/META-INF/resources/mev/html/app/js/app.js
+++ src/main/webapp/META-INF/resources/mev/html/app/js/app.js
@@ -6,38 +6,36 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
 	  
 	$routeProvider.when('/home', {
-		templateUrl: 'partials/home-view.html', 
-		controller: 'HomeCtrl'});
+		templateUrl: 'partials/home-view.html'
+		});
 		
 	$routeProvider.when('/about', {
-		templateUrl: 'partials/about-view.html', 
-		controller: 'AboutCtrl'});
+		templateUrl: 'partials/about-view.html'});
 		
 	$routeProvider.when('/analyze', {
 		templateUrl: 'partials/analyze-view.html', 
 		controller: 'AnalyzeCtrl'});
 		
 	$routeProvider.when('/features', {
-		templateUrl: 'partials/features-view.html', 
-		controller: 'FeaturesCtrl'});
+		templateUrl: 'partials/features-view.html'});
 		
 	$routeProvider.when('/news', {
-		templateUrl: 'partials/news-view.html', 
-		controller: 'NewsCtrl'});
+		templateUrl: 'partials/news-view.html'});
 		
 	$routeProvider.when('/help', {
-		templateUrl: 'partials/help-view.html', 
-		controller: 'HelpCtrl'});
+		templateUrl: 'partials/help-view.html'});
 		
+	$routeProvider.when('/tutorial', {
+		templateUrl: 'partials/tutorial-view.html'});
+
 	$routeProvider.when('/heatmap/:matrixLocation', {
 		templateUrl: 'partials/heatmap-view.html', 
 		controller: 'HeatmapCtrl'});
-	$routeProvider.when('/upload', {
-		templateUrl: 'partials/upload.html', 
-		controller: 'UploadCtrl'});
+		
 	$routeProvider.when('/geneselect/:dataset', {
 		templateUrl: 'partials/genelib-view.html', 
 		controller: 'GeneSelectCtrl'});
+		
     $routeProvider.otherwise({redirectTo: '/home'});
 
   }]);
