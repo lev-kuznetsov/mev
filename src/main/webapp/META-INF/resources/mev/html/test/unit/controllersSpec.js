848b6dad6bf86587d37a126a8163b62799e05d14
Jalil F <jrab227@gmail.com>
2013-07-25 17:54:22 -0400

    Layout for testing completed

--- src/main/webapp/META-INF/resources/mev/html/test/unit/controllersSpec.js
+++ src/main/webapp/META-INF/resources/mev/html/test/unit/controllersSpec.js
@@ -197,45 +197,6 @@ describe('myApp controllers', function(){
       $rootScope = $injector.get('$rootScope');
       var $controller = $injector.get('$controller');
       
-      $httpBackend.whenPOST('data/undefined/marks', 'blank').respond(201, '');
-      
-      $httpBackend.whenGET('data/undefined/marks').respond({test: 'test'});
-      $httpBackend.whenGET('data/undefined-2').respond({genes: 'test'});
-      $httpBackend.whenGET('data/undefined-1')
-                  .respond(
-                     {genes:[
-
-                       {
-                         name:"test1",
-                         symbol:"TEST1",
-                         description:"This is the test1",
-                         ensembleId: "TEST1ID",
-                         pathways: "Test, Test, test"
-                       },
-                       {
-                         name:"test2",
-                         symbol:"TEST2",
-                         description:"This is the test2",
-                         ensembleId: "TEST2ID",
-                         pathways: "Test2, Test2, test2"
-                       } 
-
-                     ],
-                     maxPage: 2,
-                     nearbyPages: [
-                        {
-                          number: 1,
-                          link: "testdataset-1"
-					    },
-					    {
-                          number: 2,
-                          link: "testdataset-2"
-					    },
-					  ]
-                    });
-                  
-
-
       createController = function() {
 		  
           $controller('GeneSelectCtrl', {'$scope': $rootScope});
@@ -252,105 +213,62 @@ describe('myApp controllers', function(){
     });
 
     
-    it('should send a request to download the first page of data and update vars', function() {
-       
-       $httpBackend.expectGET('data/undefined-1');
-       var controller = createController();
-       $httpBackend.flush();
+    it('should send a request to download the initial page of data and update vars', function() {
+        var controller = createController();
+        $httpBackend.flush();
+    });
 
-       expect($rootScope.genesCurrentPage.length).toBe(2);
-       expect($rootScope.pageMax).toBe(2);
-       expect($rootScope.currentPage).toBe(1);
-       expect($rootScope.nearbyPages.length).toBe(2);
-       
+    it('should fail gracefully when a request for page is not available', function() {
+
     });
     
-    it('should update marked genes when genesMarked is called', function () {
+    it('should update marked genes when genes are marked by user', function () {
       
-      var controller = createController();
-      $httpBackend.flush();
+        var controller = createController();
+        $httpBackend.flush();
       
-      $httpBackend.expectGET('data/undefined/marks').respond({test: 'test'});
-      $rootScope.getMarks();
-      $httpBackend.flush();
-      
-      expect($rootScope.genesMarked.test).toBe('test');
-  
+    });
+
+    it('should update marked genes when user asks to remove genes is called', function() {
+
+        var controller = createController();
+        $httpBackend.flush();
+    });
+    
+    it('should update genes list when filter is applied', function() {
+
+        var controller = createController();
+        $httpBackend.flush();
     });
 	
-    it('should update genesSearched when searchGene is called', function() {
+    it('should fail gracefully when filter request is sent and no objects are received', function() {
         var controller = createController();
         $httpBackend.flush();
-        
-        $httpBackend.expectGET('data/undefined/q=test').respond({test: 'test'});
-        $rootScope.searchGene('test');
-        $httpBackend.flush();
-        
-        expect($rootScope.genesSearched.test).toBe('test');
-    });
-    
-    it('should page up and page down when respective functions are called', function() {
-		
-        var controller = createController();
-        $httpBackend.flush();
-        
-        expect($rootScope.currentPage).toBe(1);
-        $httpBackend.expectGET('data/undefined-2').respond({genes: 'test'});
-        $rootScope.pageUp();
-        $httpBackend.flush();
-        
-        expect($rootScope.currentPage).toBe(2);
-        
-        $httpBackend.expectGET('data/undefined-1').respond({genes: 'test'});
-        $rootScope.pageDown();
-        $httpBackend.flush();
-        
-        expect($rootScope.currentPage).toBe(1);
-        
-    });
-    
-    it('should not update page up when currently on page 1', function() {
-		
-        var controller = createController();
-        $httpBackend.flush();
-        
-        expect($rootScope.currentPage).toBe(1);
-        
-        $rootScope.pageDown();
-        
-        expect($rootScope.currentPage).toBe(1);
-        
-    });
-    
-    it('should not update page down when currently on max page', function() {
-		
-        var controller = createController();
-        $httpBackend.flush();
-        
-        expect($rootScope.currentPage).toBe(1);
-        $httpBackend.expectGET('data/undefined-2').respond({genes: 'test'});
-        $rootScope.pageUp();
-        $httpBackend.flush();
-        
-        expect($rootScope.currentPage).toBe(2);
-        $rootScope.pageUp();
-        
-        expect($rootScope.currentPage).toBe(2);
-    });
-    
-    it('should update genesMarked when addMark is called', function() {
 
+    });
+    
+    it('should page up when user requests', function() {
         var controller = createController();
         $httpBackend.flush();
-        
+    
+    });
 
-        $httpBackend.expectPOST('data/undefined/marks', 'blank').respond(201, '');
-        $rootScope.addMark('blank');
-        
-        $httpBackend.expectGET('data/undefined/marks').respond({test: 'test'});
+    it('should page down when user requests', function() {
+        var controller = createController();
         $httpBackend.flush();
-        
-        expect($rootScope.genesMarked.test).toBe('test');
+
+    });
+    
+    it('should not update page down when currently on page 1', function() {
+        var controller = createController();
+        $httpBackend.flush();
+		
+    });
+    
+    it('should not update page up when currently on max page', function() {
+        var controller = createController();
+        $httpBackend.flush();
+		
     });
     
   });
