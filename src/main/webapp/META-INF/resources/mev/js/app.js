2d714a9b024091a2349728c684b0d47e7bf9dcc4
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2013-08-09 11:05:58 -0400

    Fixed the partials view issue, added log statement for data fetch

--- src/main/webapp/META-INF/resources/mev/js/app.js
+++ src/main/webapp/META-INF/resources/mev/js/app.js
@@ -6,34 +6,34 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
 	  
 	$routeProvider.when('/home', {
-		templateUrl: '/views-partials-home'
+		templateUrl: '/views/partials/home'
 		});
 		
 	$routeProvider.when('/about', {
-		templateUrl: '/views-partials-about'});
+		templateUrl: '/views/partials/about'});
 		
 	$routeProvider.when('/analyze', {
-		templateUrl: '/views-partials-analyze', 
+		templateUrl: '/views/partials/analyze', 
 		controller: 'AnalyzeCtrl'});
 		
 	$routeProvider.when('/features', {
-		templateUrl: '/views-partials-features'});
+		templateUrl: '/views/partials/features'});
 		
 	$routeProvider.when('/news', {
-		templateUrl: '/views-partials-news'});
+		templateUrl: '/views/partials/news'});
 		
 	$routeProvider.when('/help', {
-		templateUrl: '/views-partials-help'});
+		templateUrl: '/views/partials/help'});
 		
 	$routeProvider.when('/tutorial', {
-		templateUrl: '/views-partials-tutorial'});
+		templateUrl: '/views/partials/tutorial'});
 
 	$routeProvider.when('/heatmap/:matrixLocation', {
-		templateUrl: '/views-partials-heatmap', 
+		templateUrl: '/views/partials/heatmap', 
 		controller: 'HeatmapCtrl'});
 		
 	$routeProvider.when('/geneselect/:dataset', {
-		templateUrl: '/views-partials-genelib', 
+		templateUrl: '/views/partials/genelib', 
 		controller: 'GeneSelectCtrl'});
 		
     $routeProvider.otherwise({redirectTo: '/home'});
