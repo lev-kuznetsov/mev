ed6f60d20c91164ee8800097b57e43883bbc0d83
Jalil F <jrab227@gmail.com>
2013-10-03 16:27:05 -0400

    Added top heatmap and separated top from left logic. fixes #143

--- src/main/webapp/META-INF/resources/mev/js/directives/visHeatmapDrct.js
+++ src/main/webapp/META-INF/resources/mev/js/directives/visHeatmapDrct.js
@@ -250,15 +250,162 @@ drct.directive('visHeatmap', [function() {
 							})
 						});
 
+				if (newdata.tree.left) {
 					
-				if (newdata.tree) { //only goes here if tree information is built into system
-					
-					var treewidth = margin.left;
-					var treeheight = height;
-					var genes = new Array();
-					
-					var getter = function(node) {
+					var lefttreewidth = margin.left;
+					var lefttreeheight = height;
+					var leftgenes = new Array();
+
+					var leftcluster = d3.layout.cluster()
+						.size([lefttreeheight, lefttreewidth -120])
+						.separation(function(a,b){ //Define a separation of neighboring nodes. Make neighbor distances equidistant so they can align with heatmap.
+							return a.parent == b.parent ? 5:5;
+					});
+
+					var leftelbow = function(d, i){
+						return "M" + (lefttreewidth - (d.source.distance * 100) )  + "," + (d.source.x + margin.top )
+						+ "V" + ( d.target.x + margin.top  ) + "H" + (lefttreewidth - (d.target.distance * 100) );
+					};
+
+					var leftclick = function(d){
+
+						var nColor = '#ffffff'; //Initial nonselected color of a node.
+						var pColor = '#cccccc'; //Initial nonselected color of a branch.
 						
+						var cir = lefttree //Selects all the circles representing nodes but only those which were the clicked circle, using datum as the equality filter.
+							.selectAll("circle")
+							.filter(function(db){
+								return d === db ? 1 : 0;
+							});
+
+						var path = lefttree.selectAll(".leftlink") //Selects all paths but only those which have the same source coordinates as the node clicked.
+							.filter(function(dp){
+								return (d.x === dp.source.x && d.y === dp.source.y) ? 1 : 0;
+							});
+							
+						//Check the state of the clicked node. If 'active' (color is green) swap to inactive colors and pass those colors down to all children and vice versa.
+						if(cir.style('fill') == '#00ff00'){
+						
+							cir.style('fill', nColor)
+								.transition().attr('r', 2).duration(500); //Change radius of nonactive nodes.
+							
+							path.transition().style('stroke', pColor).duration(500);
+						
+						} else {
+							
+							nColor = '#00ff00';
+							pColor = '#00ff00';
+							cir.style('fill', nColor)
+								.transition().attr('r', 5).duration(500);
+							path.transition().style('stroke', pColor).duration(500);
+							
+						};
+
+						if(d.children){ //Check if the node clicked is not a leaf. If the node has children, travel down the three updating the colors to indicate selection.
+							leftwalk(d, nColor, pColor);
+						} else {
+							if(nColor == '#00ff00'){ //Check color to see if indicated action is a select/deselect
+								if(leftgenes.indexOf(d.name) == -1){ //Check if gene already is in the array.
+									leftgenes.push(d.name)
+								}
+							} else { //Algorithm for removing genes from the list on a deselect.
+								var index = leftgenes.indexOf(d.name); //Get the index of the given gene in the gene array.
+								leftgenes.splice(index, 1); //Splice that gene out of the array using its gotten index.
+							};
+						};
+						
+						//alert(leftgenes) <-- Function to do something with selected genes.
+						
+					};
+
+					//Function to walk down the tree from a selected node and apply proper color assignments based on selection.
+					var leftwalk = function(d, nColor, pColor){
+						//alert(d.name);
+						d.children.forEach(function(dc){ //Loop through each child, recursively calling walk() as necessary.
+						
+							lefttree
+								.filter(function(db){
+									return dc === db ? 1 : 0;
+								})
+								.transition().style("fill",nColor).duration(500)
+								.transition().attr("r", 2).duration(500);
+
+							d3.selectAll(".leftlink")
+								.filter(function(dp){
+									return (dc.x === dp.source.x && dc.y === dp.source.y) ? 1 : 0;
+								})
+								.transition().style("stroke", pColor).duration(500);
+
+							if(dc.children){ //Check if children exist, if so, recurse the previous function.
+								leftwalk(dc, nColor, pColor);
+							} else {
+								if(nColor == '#00ff00'){
+									if(leftgenes.indexOf(dc.name) == -1){
+										leftgenes.push(dc.name);
+									};
+								} else {
+									var index = leftgenes.indexOf(dc.name);
+									leftgenes.splice(index, 1);
+								}
+							};
+						});
+					};
+
+					var leftnodes = leftcluster.nodes(newdata.tree.left)//Create the nodes based on the tree structure.
+
+					var lefttree = cellcover.append("g").attr("class", "lefttree");
+
+					var leftlink = lefttree.selectAll("path") //Create the branches.
+						.data(leftcluster.links(leftnodes))
+						.enter().append("path")
+						.attr("class", "leftlink")
+						.attr("d", leftelbow) //Call function elbow() so that the paths drawn are straight and not curved.
+
+
+					var leftnode = lefttree.selectAll("circle") //Take the data in nodes and create individual nodes.
+						.data(leftnodes)
+						.enter().append("circle")
+						.attr("class","leftnode")
+						.attr("cx", function(d) {
+							if( !(d.parent) ){
+								return Math.floor( (lefttreewidth - (d.distance * 100)) );
+							} else {
+								if( !(d.children) ){
+									return  Math.floor(lefttreewidth) ;
+								} else {
+									return Math.floor(lefttreewidth - (d.distance * 100));
+								}
+							}
+						})
+						.attr("cy", function(d) {
+							if( !(d.parent) ){
+								return Math.floor(d.x) + margin.top;
+							} else {
+								if( !(d.children) ){
+									return Math.floor(d.x) + margin.top;
+								} else {
+									return Math.floor(d.x) + margin.top;
+								}
+							}
+						})
+						.attr("r", 2)
+						.on("click", function(d) {
+							console.log(d);
+							console.log( getter(d) );
+						})
+						.on("click", leftclick);
+					
+				}
+					
+				if (newdata.tree.top) { //only goes here if tree information is built into system
+					
+					var toptreewidth = width;
+					var toptreeheight = margin.top;
+					
+					var topgenes = new Array();
+
+					var getter = function(node) {
+
 						if (!node.children) {
 							return(node.name);
 						} else {
@@ -266,139 +413,151 @@ drct.directive('visHeatmap', [function() {
 								getter(node.children[i])
 							}
 						}
-							
+
 					}
-					
-					var cluster = d3.layout.cluster()
-						.size([treeheight, treewidth -120])
+
+					var topcluster = d3.layout.cluster()
+						.size([toptreewidth, toptreeheight -120 ])
 						.separation(function(a,b){ //Define a separation of neighboring nodes. Make neighbor distances equidistant so they can align with heatmap.
 							return a.parent == b.parent ? 5:5;
 					});
-					
-					var elbow = function(d, i){
-						return "M" + (treewidth - (d.source.distance * 100))  + "," + d.source.x
-						+ "V" + d.target.x + "H" + (treewidth - (d.target.distance * 100));
+
+					var topelbow = function(d, i){
+						return "M" + (d.target.x + margin.left )  + "," + (toptreeheight - (d.target.distance * 100) )
+						+ "V" + (toptreeheight - (d.source.distance * 100) ) + "H" +  ( d.source.x + margin.left  );
 					};
 					
-					var click = function(d){
+				
+
+					var topclick = function(d){
+
 						var nColor = '#ffffff'; //Initial nonselected color of a node.
 						var pColor = '#cccccc'; //Initial nonselected color of a branch.
 						
-						var cir = d3.selectAll("svg") //Selects all the circles representing nodes but only those which were the clicked circle, using datum as the equality filter.
+						var cir = toptree //Selects all the circles representing nodes but only those which were the clicked circle, using datum as the equality filter.
 							.selectAll("circle")
 							.filter(function(db){
 								return d === db ? 1 : 0;
-						});
-						
-						var path = d3.selectAll(".link") //Selects all paths but only those which have the same source coordinates as the node clicked.
+							});
+
+						var path = toptree.selectAll(".toplink") //Selects all paths but only those which have the same source coordinates as the node clicked.
 							.filter(function(dp){
 								return (d.x === dp.source.x && d.y === dp.source.y) ? 1 : 0;
 							});
+							
 						//Check the state of the clicked node. If 'active' (color is green) swap to inactive colors and pass those colors down to all children and vice versa.
 						if(cir.style('fill') == '#00ff00'){
-								cir.style('fill', nColor)
-									.transition().attr('r', 2).duration(500); //Change radius of nonactive nodes.
-								path.transition().style('stroke', pColor).duration(500);
-						}
-						else{
+						
+							cir.style('fill', nColor)
+								.transition().attr('r', 2).duration(500); //Change radius of nonactive nodes.
+							
+							path.transition().style('stroke', pColor).duration(500);
+						
+						} else {
+							
 							nColor = '#00ff00';
 							pColor = '#00ff00';
 							cir.style('fill', nColor)
 								.transition().attr('r', 5).duration(500);
 							path.transition().style('stroke', pColor).duration(500);
+							
 						};
-						
+
 						if(d.children){ //Check if the node clicked is not a leaf. If the node has children, travel down the three updating the colors to indicate selection.
-							walk(d, nColor, pColor);
-						}
-						else{
+							topwalk(d, nColor, pColor);
+						} else {
 							if(nColor == '#00ff00'){ //Check color to see if indicated action is a select/deselect
-								if(genes.indexOf(d.name) == -1){ //Check if gene already is in the array.
-									genes.push(d.name)
+								if(topgenes.indexOf(d.name) == -1){ //Check if gene already is in the array.
+									topgenes.push(d.name)
 								}
-							}
-							else{ //Algorithm for removing genes from the list on a deselect.
-								var index = genes.indexOf(d.name); //Get the index of the given gene in the gene array.
-								genes.splice(index, 1); //Splice that gene out of the array using its gotten index.
+							} else { //Algorithm for removing genes from the list on a deselect.
+								var index = topgenes.indexOf(d.name); //Get the index of the given gene in the gene array.
+								topgenes.splice(index, 1); //Splice that gene out of the array using its gotten index.
 							};
 						};
-						scope.celllink["row"] = genes; //adds selected genes to cell links 
+						
+						//alert(topgenes) //<-- Function to do something with selected genes.
+						
 					};
+
 					//Function to walk down the tree from a selected node and apply proper color assignments based on selection.
-					var walk = function(d, nColor, pColor){
+					var topwalk = function(d, nColor, pColor){
 						//alert(d.name);
 						d.children.forEach(function(dc){ //Loop through each child, recursively calling walk() as necessary.
-							d3.selectAll("svg")
+						
+							toptree
 								.selectAll("circle")
 								.filter(function(db){
 									return dc === db ? 1 : 0;
 								})
 								.transition().style("fill",nColor).duration(500)
 								.transition().attr("r", 2).duration(500);
-							
-							d3.selectAll(".link")
+
+							d3.selectAll(".toplink")
 								.filter(function(dp){
 									return (dc.x === dp.source.x && dc.y === dp.source.y) ? 1 : 0;
 								})
 								.transition().style("stroke", pColor).duration(500);
-								
+
 							if(dc.children){ //Check if children exist, if so, recurse the previous function.
-								walk(dc, nColor, pColor);
-							}
-							else{
+								topwalk(dc, nColor, pColor);
+							} else {
 								if(nColor == '#00ff00'){
-									if(genes.indexOf(dc.name) == -1){
-										genes.push(dc.name);
+									if(topgenes.indexOf(dc.name) == -1){
+										topgenes.push(dc.name);
 									};
-								}
-								else{
-									var index = genes.indexOf(dc.name);
-									genes.splice(index, 1);
+								} else {
+									var index = topgenes.indexOf(dc.name);
+									topgenes.splice(index, 1);
 								}
 							};
 						});
 					};
-					
-					var nodes = cluster.nodes(newdata.tree)//Create the nodes based on the tree structure.
-					 
-					var link = cellcover.selectAll("path") //Create the branches.
-						.data(cluster.links(nodes))
+
+					var topnodes = topcluster.nodes(newdata.tree.top)//Create the nodes based on the tree structure.
+
+					var toptree = cellcover.append("g").attr("class", "toptree");
+
+					var toplink = toptree.selectAll("path") //Create the branches.
+						.data(topcluster.links(topnodes))
 						.enter().append("path")
-						.attr("class", "link")
-						.attr("d", elbow) //Call function elbow() so that the paths drawn are straight and not curved.
-						
-			 
-					var node = cellcover.selectAll("circle") //Take the data in nodes and create individual nodes.
-						.data(nodes)
+						.attr("class", "toplink")
+						.attr("d", topelbow) //Call function topelbow() so that the paths drawn are straight and not curved.
+
+
+					var topnode = toptree.selectAll("circle") //Take the data in nodes and create individual nodes.
+						.data(topnodes)
 						.enter().append("circle")
-						.attr("class","node")
-						.attr("cx", function(d) {
+						.attr("class","topnode")
+						.attr("cy", function(d) {
 							if( !(d.parent) ){
-								return Math.floor( (treewidth - (d.distance * 100)) );
+								return Math.floor( (toptreeheight - (d.distance * 100)) );
 							} else {
 								if( !(d.children) ){
-									return  Math.floor(treewidth) ;
+									return  Math.floor(toptreeheight) ;
 								} else {
-									return Math.floor(treewidth - (d.distance * 100));
+									return Math.floor(toptreeheight - (d.distance * 100));
 								}
 							}
 						})
-						.attr("cy", function(d) {
+						.attr("cx", function(d) {
 							if( !(d.parent) ){
-								return Math.floor(d.x);
+								return Math.floor(d.x) + margin.left;
 							} else {
 								if( !(d.children) ){
-									return Math.floor(d.x);
+									return Math.floor(d.x) + margin.left;
 								} else {
-									return Math.floor(d.x) ;
+									return Math.floor(d.x) + margin.left;
 								}
 							}
 						})
 						.attr("r", 2)
-						.on("click", click)
-						//.on("click", function(d) {
-						//	getter(d)
-						//}) Not sure what this does
+						.on("click", function(d) {
+							console.log(d);
+							console.log( getter(d) );
+						})
+						.on("click", topclick);
+					
 				}
 				
 			});
