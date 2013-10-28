a7b93689ce0808d253bf0e50a2128ef4fdb19bd7
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2013-10-28 12:12:31 -0400

    A little more meat for the skeleton

---
+++ heatmap/client/src/main/javascript/edu/dfci/cccb/mev/heatmap/client/javascript/main.js
@@ -0,0 +1,10 @@
+/* Use requirejs to load other modules located under this package, refer to client configuration annotation
+ * located at /edu/dfci/cccb/mev/heatmap/package-info.java for url mappings.
+ * 
+ * For example, for:
+ *
+ * @Client (resources = @Resource (export = "/resources/heatmap/javascript",
+ *                                 classpath = "/edu/dfci/cccb/mev/heatmap/javascript"))
+ *
+ * this file (main.js) will be located at /resources/heatmap/javascript/main.js
+ */
