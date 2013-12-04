532efb56b61b785482abe8fa3d243a9a3987907f
Jalil F <jrab227@gmail.com>
2013-12-04 13:32:19 -0500

    Added hcl clustering to api protocol. fixes #369

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/services.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/services.js
@@ -1,126 +1,158 @@
-define ([ 'angular', 'd3' ], function (angular, d3) {
+define(
+		[ 'angular', 'd3' ],
+		function(angular, d3) {
 
-  return angular.module ('myApp.services', []).value ('appVersion', '0.1')
-      .value ('appName', 'MEV: Multi-Experiment Viewer').factory (
-          'mainMenuBarOptions', [ function () {
-            return [ {
-              value : "About",
-              url : "#"
-            }, {
-              value : "Contact",
-              url : "#"
-            } ];
-          } ]).factory ('analysisOptions', [ function () {
+			return angular
+					.module('myApp.services', [])
+					.value('appVersion', '0.1')
+					.value('appName', 'MEV: Multi-Experiment Viewer')
+					.factory('mainMenuBarOptions', [ function() {
+						return [ {
+							value : "About",
+							url : "#"
+						}, {
+							value : "Contact",
+							url : "#"
+						} ];
+					} ])
+					.factory('analysisOptions', [ function() {
 
-        // future feature
+						// future feature
 
-        return undefined;
-        
-      } ])
-      .factory ('heatmapGenerator', [function () {
-        return null;
-      }])
-      .factory ('QHTTP', [ '$http', '$q', function ($http, $q) {
+						return undefined;
 
-        return function (params, callback_fn, error_fn) {
-          var deferred = $q.defer ();
+					} ])
+					.factory('heatmapGenerator', [ function() {
+						return null;
+					} ])
+					.factory('QHTTP', [ '$http', '$q', function($http, $q) {
 
-          $http (params).success (function (data, status) {
+						return function(params, callback_fn, error_fn) {
+							var deferred = $q.defer();
 
-            deferred.resolve (callback_fn (data, status));
+							$http(params).success(function(data, status) {
 
-          }).error (function (data, status) {
+								deferred.resolve(callback_fn(data, status));
 
-            deferred.reject (error_fn (data, status));
+							}).error(function(data, status) {
 
-          });
+								deferred.reject(error_fn(data, status));
 
-          return deferred.promise;
-        };
+							});
 
-      } ])
-      .factory ('API', [ 'QHTTP', function (QHTTP) {
+							return deferred.promise;
+						};
 
-        return {
+					} ])
+					.factory(
+							'API',
+							[
+									'QHTTP',
+									function(QHTTP) {
 
-          heatmap : {
-            get : function (url) {
-              var params = {
-                method : 'GET',
-                url : 'heatmap/' + url + '?format=json',
-                format : 'json'
-              };
-              return QHTTP (params, function (d, s) {
-                return d;
-              }, function (d, s) {
-               
-                return d, s;
-              });
+										return {
 
-            }
-          },
-          user: {
-            datasets: {
-              get: function(){
-                
-                var params = {
-                    method : 'GET',
-                    url : '/dataset?format=json'
-                  };
-                  return QHTTP (params, function (d, s) {
-                    return d;
-                  }, function (d, s) {
-                   
-                    return d, s;
-                  });
-                
-              }
-            }
-          },
-          dataset: {
-            get: function(url){
-              
-              var params = {
-                  method : 'GET',
-                  url : '/dataset/' + url  + '?format=json'
-                };
-              
-                return QHTTP (params, function (d, s) {
-                  return d;
-                }, function (d, s) {
-                 
-                  return d, s;
-                });
-              
-            }
-          },
-          hcl: {
-            radial: {
-              get: function(url){
-                
-                var params = {
-                    method : 'GET',
-                    url : 'heatmap/' + url + '?format=newick',
-                    format : 'json'
-                  };
-                  return QHTTP (params, function (d, s) {
-                    
-                    return d3.text('heatmap/' + url + '?format=newick', function(data) {return data})
-                    
-                  }, function (d, s) {
-                    return d, s;
-                  });
+											heatmap : {
+												get : function(url) {
+													var params = {
+														method : 'GET',
+														url : 'heatmap/'
+																+ url
+																+ '?format=json',
+														format : 'json'
+													};
+													return QHTTP(params,
+															function(d, s) {
+																return d;
+															}, function(d, s) {
 
-              }
-            },
-            linear :{
-              get: function(url){
-                return null;
-              }
-            }
-          }
+																return d, s;
+															});
 
-        }
-      } ]);
+												}
+											},
+											user : {
+												datasets : {
+													get : function() {
 
-});
+														var params = {
+															method : 'GET',
+															url : '/dataset?format=json'
+														};
+														return QHTTP(
+																params,
+																function(d, s) {
+																	return d;
+																},
+																function(d, s) {
+
+																	return d, s;
+																});
+
+													}
+												}
+											},
+											dataset : {
+												get : function(url) {
+
+													var params = {
+														method : 'GET',
+														url : '/dataset/'
+																+ url
+																+ '?format=json'
+													};
+
+													return QHTTP(params,
+															function(d, s) {
+																return d;
+															}, function(d, s) {
+
+																return d, s;
+															});
+
+												}
+											},
+											analysis : {
+												hcl : {
+													row : {
+														get : function(url) {
+
+															var params = {
+																method : 'GET',
+																url : 'analysis/'
+																		+ url
+																		+ '/hcl/row',
+																format : 'newick'
+															};
+															return QHTTP(
+																	params,
+																	function(d,
+																			s) {
+
+																		return d3
+																				.text(
+																						'heatmap/'
+																								+ url
+																								+ '?format=newick',
+																						function(
+																								data) {
+																							return data
+																						})
+
+																	},
+																	function(d,
+																			s) {
+																		return
+																				d,
+																				s;
+																	});
+
+														}
+													}
+												}
+											}
+
+										}
+									} ]);
+
+		});
