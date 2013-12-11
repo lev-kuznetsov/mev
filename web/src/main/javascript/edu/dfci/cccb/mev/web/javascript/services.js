9de635fa216341abc92548bba13b7a2507c66683
Jalil F <jrab227@gmail.com>
2013-12-11 13:11:31 -0500

    Added random string generator.

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/services.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/services.js
@@ -18,6 +18,17 @@ define(
 					.factory('analysisOptions', function(){
 						
 					})
+					.factory('pseudoRandomStringGenerator', function(length){
+					  
+					  var text = "";
+				    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+				    for( var i=0; i < length; i++ )
+				        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+				    return text;
+            
+          })
 					.factory('alertService', [ function() {
 
 						return {
