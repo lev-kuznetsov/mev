47bf114875e85705d9b715bdbaa67a71ed6990fd
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2014-01-22 21:34:11 -0500

    Added caching to DB backed value store dramatically improving clustering performance.

--- dataset/domain/src/main/java/edu/dfci/cccb/mev/dataset/domain/metamodel/MetamodelBackedValueStoreBuilder.java
+++ dataset/domain/src/main/java/edu/dfci/cccb/mev/dataset/domain/metamodel/MetamodelBackedValueStoreBuilder.java
@@ -42,6 +42,7 @@ import edu.dfci.cccb.mev.dataset.domain.contract.ValueStoreBuilder;
 import edu.dfci.cccb.mev.dataset.domain.contract.ValueStoreException;
 import edu.dfci.cccb.mev.dataset.domain.contract.Values;
 import edu.dfci.cccb.mev.dataset.domain.prototype.AbstractValueStoreBuilder;
+import edu.dfci.cccb.mev.dataset.domain.simple.CachedValues;
 
 /**
  * @author levk
@@ -89,6 +90,6 @@ public class MetamodelBackedValueStoreBuilder extends AbstractValueStoreBuilder 
   @Override
   @Synchronized
   public Values build () {
-    return store;
+    return new CachedValues (store);
   }
 }
