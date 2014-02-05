669d755772965c226939bd4e2a61a7de2ffb84fb
apartensky <apartensky@gmail.com>
2014-02-04 21:05:42 -0500

    Fix-Remove Large File - save all files except the large one to new branch

--- annotation/server/src/main/java/com/google/refine/commands/project/ViewPresetAnnotationsCommand.java
+++ annotation/server/src/main/java/com/google/refine/commands/project/ViewPresetAnnotationsCommand.java
@@ -1,10 +1,22 @@
 package com.google.refine.commands.project;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +27,25 @@ import org.slf4j.LoggerFactory;
 
 import com.google.refine.ProjectManager;
 import com.google.refine.ProjectMetadata;
+import com.google.refine.browsing.Engine;
+import com.google.refine.browsing.FilteredRows;
+import com.google.refine.browsing.RowVisitor;
 import com.google.refine.commands.Command;
+import com.google.refine.model.Column;
 import com.google.refine.model.Project;
+import com.google.refine.model.Row;
+import com.google.refine.operations.row.ImportPresetsRowRemovalOperation;
+import com.google.refine.process.Process;
 import com.google.refine.util.ParsingUtilities;
 
+import edu.dfci.cccb.mev.dataset.domain.contract.Dataset;
+import edu.dfci.cccb.mev.dataset.domain.contract.DatasetBuilderException;
+import edu.dfci.cccb.mev.dataset.domain.contract.InvalidDatasetNameException;
+import edu.dfci.cccb.mev.dataset.domain.contract.InvalidDimensionTypeException;
+import edu.dfci.cccb.mev.dataset.domain.contract.RawInput;
+import edu.dfci.cccb.mev.dataset.domain.contract.Selection;
+import edu.dfci.cccb.mev.dataset.domain.simple.SimpleSelection;
+import edu.dfci.cccb.mev.dataset.rest.assembly.tsv.UrlTsvInput;
 import edu.dfci.cccb.mev.presets.contract.Preset;
 import edu.dfci.cccb.mev.presets.contract.PresetDescriptor;
 import freemarker.template.utility.NullArgumentException;
@@ -28,7 +55,7 @@ public class ViewPresetAnnotationsCommand extends Command {
   final static protected Logger logger = LoggerFactory.getLogger("ViewPresetAnnotationsCommand");
   
   @Override
-  public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  public void doGet (final HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     // TODO Auto-generated method stub
     ProjectManager.getSingleton().setBusy(true);
     try {
@@ -58,6 +85,76 @@ public class ViewPresetAnnotationsCommand extends Command {
                     pm.setName(projectName);
                 }
             }
+            
+          //ap
+            //(try (BufferedReader reader = new BufferedReader(new FileReader (file))){)
+            Path pathDataFile = Paths.get(descriptor.dataUrl ().toURI ());
+            try(BufferedReader reader = Files.newBufferedReader (pathDataFile, Charset.defaultCharset ())){
+              String sHeader = reader.readLine ();
+              if(sHeader!=null){
+                String[] arHeader = sHeader.split ("\t");
+                final ArrayList<String> listHeader = new ArrayList<String>(Arrays.asList(arHeader));
+                final Map<String, String> mapHeader = new HashMap <String, String>(listHeader.size ());
+                for(String columnName : listHeader)
+                  if(!columnName.trim ().equals ("")) mapHeader.put(columnName, null);
+                
+                Project project = ProjectManager.getSingleton().getProject (projectID);
+                
+                final Engine engine = getEngine (request, project);
+                final List<Integer> unmatchedRowIndices = new ArrayList<Integer>();
+                
+                RowVisitor visitor = new RowVisitor () {
+                  int rowCount = 0;
+                  Column theIdColumn;
+
+                  @Override
+                  public void start (Project project) {
+
+                    // if no id column found, assume first column is the id
+                    List<Column> columns = project.columnModel.columns;
+                    theIdColumn = columns.get (0);
+
+                  }
+
+                  @Override
+                  public boolean visit (Project project, int rowIndex, Row row) {
+                    String cellData = row.getCell (theIdColumn.getCellIndex ()).value.toString ();
+                    if (mapHeader.containsKey (cellData)) {
+//                      if(logger.isDebugEnabled ())
+//                        logger.debug ("++ will import"+cellData);
+                      rowCount++;
+                    }else{
+                      unmatchedRowIndices.add(rowIndex);
+//                      if(logger.isDebugEnabled ())
+//                        logger.debug ("-- skip import"+rowIndex);
+                    }
+                    return false;
+                  }
+
+                  @Override
+                  public void end (Project project) {                        
+                    try {
+                               
+                      ImportPresetsRowRemovalOperation op = new ImportPresetsRowRemovalOperation(getEngineConfig(request), unmatchedRowIndices);
+                      Process process = op.createProcess(project, new Properties());            
+                      project.processManager.queueProcess(process);         
+                      
+                    } catch (Exception e) {
+                      e.printStackTrace();
+                    }
+                  }
+
+                  @Override
+                  public boolean pass (Project project, int rowIndex, Row row) {                    
+                    return false;
+                  }
+                };
+                
+                FilteredRows filteredRows = engine.getAllFilteredRows ();
+                filteredRows.accept (project, visitor);
+              }
+            }
+            
             redirect(response, "/annotations/import-dataset/project?"
                      +"import-preset="+datasetName+"&project=" + projectID);
         } else {
--- dataset/domain/src/test/java/edu/dfci/cccb/mev/dataset/domain/simple/SimpleDatasetBuilderTest.java
+++ dataset/domain/src/test/java/edu/dfci/cccb/mev/dataset/domain/simple/SimpleDatasetBuilderTest.java
@@ -16,13 +16,26 @@ package edu.dfci.cccb.mev.dataset.domain.simple;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.dfci.cccb.mev.dataset.domain.contract.Dataset;
+import edu.dfci.cccb.mev.dataset.domain.contract.DatasetBuilderException;
+import edu.dfci.cccb.mev.dataset.domain.contract.InvalidDatasetNameException;
+import edu.dfci.cccb.mev.dataset.domain.contract.InvalidDimensionTypeException;
+import edu.dfci.cccb.mev.dataset.domain.contract.RawInput;
+import edu.dfci.cccb.mev.dataset.domain.contract.Selection;
 import edu.dfci.cccb.mev.dataset.domain.mock.MapBackedValueStoreBuilder;
 import edu.dfci.cccb.mev.dataset.domain.mock.MockTsvInput;
+import edu.dfci.cccb.mev.dataset.domain.mock.MockUrlInput;
 import edu.dfci.cccb.mev.dataset.domain.supercsv.SuperCsvParserFactory;
 
 /**
@@ -40,7 +53,7 @@ public class SimpleDatasetBuilderTest {
     builder.setValueStoreBuilder (new MapBackedValueStoreBuilder ());
   }
 
-  @Test
+  @Test @Ignore
   public void simpleBuild () throws Exception {
     Dataset set = builder.build (new MockTsvInput ("mock", "id\tsa\tsb\tsc\n" +
                                                            "g1\t.1\t.2\t.3\n" +
@@ -48,4 +61,66 @@ public class SimpleDatasetBuilderTest {
     assertEquals ("mock", set.name ());
     assertEquals (0.1, set.values ().get ("g1", "sa"), 0.0);
   }
+  
+  @Test 
+  public void buildWithSelection() throws DatasetBuilderException, InvalidDatasetNameException, InvalidDimensionTypeException{
+    
+    List<String> keys = new ArrayList<String> (){
+      {
+        add("66a354fe-454e-40a6-8464-a7d97236d398_miR_gene_expression");
+        add("9635a2c4-fd8b-4b4c-9338-def32b6dd8d3_miR_gene_expression");
+        add("1e1ae4dd-1f89-42bd-8154-165f603962b9_miR_gene_expression");
+        add("b3794e62-c4bc-43b1-97ca-b6f6e01aa026_miR_gene_expression");
+        add("c986ab6f-912a-424f-9957-ff34452339c5_miR_gene_expression");
+        add("25a9fd1a-0ff1-434b-bc4b-b78834750994_miR_gene_expression");
+        add("b5a63033-f14b-4040-b2b2-8acf9c9ba6d4_miR_gene_expression");
+        add("6a8f9385-2064-4d8c-9af1-d2bed53afebb_miR_gene_expression");
+        add("e4fe4b0b-198a-46b2-adb3-2f0b140502f8_miR_gene_expression");
+      }
+    };
+    
+    Selection sourceSelection = new SimpleSelection ("dataset.with.from.selection", new Properties (), keys);    
+    URL dataUrl = this.getClass ().getResource ("/tcga/tcga_data/ACC/Level_3/ACC.IlluminaHiSeq_miRNASeq.Level_3.Expression-miRNA.readsPerMillionMapped.tsv");        
+    assertNotNull (dataUrl);
+    
+    RawInput rawInput = new MockUrlInput (dataUrl);             
+    builder.build (rawInput, sourceSelection);
+    
+  }
+  
+  @Test @Ignore
+  public void buildNoSelectionFilter5cols90K() throws DatasetBuilderException, InvalidDatasetNameException, InvalidDimensionTypeException{
+
+    URL dataUrl = this.getClass ().getResource ("/tcga/tcga_data/COAD/Level_2/COAD.AgilentG4502A_07_3.Level_2.cols5.tsv");
+    assertNotNull (dataUrl);
+    RawInput rawInput = new MockUrlInput (dataUrl);             
+    builder.build (rawInput);
+    
+  }
+ 
+  @Test @Ignore
+  public void buildWithSelection5colsOutOf180() throws DatasetBuilderException, InvalidDatasetNameException, InvalidDimensionTypeException{
+    
+    List<String> keys = new ArrayList<String> (){
+      {
+        add("TCGA-A6-2677-01A-01R-0821-07");
+        add("TCGA-AA-3560-01A-01R-0821-07");
+        add("TCGA-AA-3529-01A-02R-0821-07");
+        add("TCGA-A6-2678-01A-01R-0821-07");
+        add("TCGA-AA-3534-01A-01R-0821-07");
+       
+      }
+    };
+    
+    Selection sourceSelection = new SimpleSelection ("dataset.with.from.selection", new Properties (), keys);
+    
+    URL dataUrl = this.getClass ().getResource ("/tcga/tcga_data/COAD/Level_2/COAD.AgilentG4502A_07_3.Level_2.tsv");
+    
+    assertNotNull (dataUrl);
+    
+    RawInput rawInput = new MockUrlInput (dataUrl);             
+    builder.build (rawInput, sourceSelection);
+    
+  }
+
 }
