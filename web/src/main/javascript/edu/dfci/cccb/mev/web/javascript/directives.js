c9909646e14b011ac810397d74e27c4b98b6d312
Jalil F <jrab227@gmail.com>
2014-02-26 12:06:45 -0500

    Removed option to cluster on rows and dialogue for dimension choice. fixes #576.

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/directives.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/directives.js
@@ -349,12 +349,11 @@ define (
               link : function (scope, elems, attrs){
                 
                 scope.dimensions = [
-                  {name: "Row", value: "row"},
-                  {name:"Column", value:"column"}];
-                
+                  {name:"Column", value:"column"}];  //Column dimension forcing #576
                 
                 scope.$watch('analysisDimension', function(newval, oldval){
                   if (newval) {
+                	  
                     API.dataset.selections.get($routeP.datasetName, newval.value).then(function(d){
                       scope.selections = d;
                     })
@@ -362,13 +361,21 @@ define (
                   
                 });
                 
+                
+                //Column dimension forcing #576
+                API.dataset.selections.get($routeP.datasetName, "column").then(function(d){
+                    scope.selections = d;
+                  })
+                
+                scope.analysisDimension = {name:"Column", value:"column"};
+                
                 scope.limmaInit = function(){
                 	
                   params = {
                     dataset: $routeP.datasetName, 
                     name: scope.analysisName,
-                    dimension: scope.analysisDimension.value, 
-                    experiment: scope.analysisExperiment, 
+                    dimension: "column", //Column dimension forcing #576
+                    experiment: scope.analysisExperiment,  
                     control: scope.analysisControl,
                     callback: scope.buildPreviousAnalysisList
                 		  
@@ -382,7 +389,7 @@ define (
                 
                 function resetSelections() {
                   scope.analysisName = "";
-                  scope.analysisDimension = "";
+                  scope.analysisDimension = {name:"Column", value:"column"}; //Column dimension forcing #576 
                   scope.analysisControl = "";
                   scope.analysisExperiment = "";
                 }
