d216a6d2a9d9f0e5f92dd6e82437231d508eb2d8
Jalil F <jrab227@gmail.com>
2014-03-03 11:50:02 -0500

    Fixed X label updating on heatmap. fixes #584.

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/directives.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/directives.js
@@ -763,21 +763,24 @@ define (
                           
                           var xband = (XIndex2Pixel(1) - XIndex2Pixel(0))/2;
                           
+                          xlabels.selectAll("text").remove();
                           
-                          
-                          xlabels.selectAll("text").data( XLabel2Index.domain() )
-                           .enter().append("text")
-                          .attr('dx', -heatmapMarginTop)
-                          .attr('y', function(d){ 
-                            return XIndex2Pixel( XLabel2Index(d) ) + xband
-                           })
-                          //.style("text-anchor", "start")
-                          .attr ("transform", "rotate(-90)")
-                          
-                          .text(function(d){return d.substr(0, 8)})
-                          .style("font-size", "14px")
-                        
+                          if (XLabel2Index.domain().length <= 50) {
+                        	
+                             xlabels.selectAll("text").data( XLabel2Index.domain() )
+                               .enter().append("text")
+                               .attr('dx', -heatmapMarginTop)
+                               .attr('y', function(d){ 
+                                  return XIndex2Pixel( XLabel2Index(d) ) + xband + 7;
+                               })
+                               //.style("text-anchor", "start")
+                               .attr ("transform", "rotate(-90)")
+                               .text(function(d){return d.substr(0, 8)})
+                               .style("font-size", "14px")
 
+                        	  
+                          }
+                          
                           
 
                           yAxis.attr (
@@ -954,6 +957,8 @@ define (
                         ;
 
                         function drawHeatmap (data) {
+                        	
+                          scope.theData=data;
                           
                           var chunks = [];
                           
@@ -988,12 +993,7 @@ define (
                           
                           var stream = setInterval(dCells, 500)
                           
-                          //heatmapcells = rects.data (data.values).enter ().append ("rect");
-						              scope.theData=data;
-                          
                           drawSelections(data.column, data.row)
-
-                          //drawCells (heatmapcells);
 
                           drawLabels (xlabels, ylabels);
 
