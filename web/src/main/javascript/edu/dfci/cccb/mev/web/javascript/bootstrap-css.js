408b05c2ca9682a916f7aad7d5d3c44116742005
Jalil F <jrab227@gmail.com>
2014-03-06 13:38:49 -0500

    Added bootstrap css links

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/bootstrap-css.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/bootstrap-css.js
@@ -2,6 +2,12 @@ define([], function(){
 	    var link = document.createElement("link");
 	    link.type = "text/css";
 	    link.rel = "stylesheet";
+	    link.href = '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css';
+	    document.getElementsByTagName("head")[0].appendChild(link);
+	    
+	    link = document.createElement("link");
+	    link.type = "text/css";
+	    link.rel = "stylesheet";
 	    link.href = '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css';
 	    document.getElementsByTagName("head")[0].appendChild(link);
 	    
