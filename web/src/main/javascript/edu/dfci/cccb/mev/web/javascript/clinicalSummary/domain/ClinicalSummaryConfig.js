0fa747fa4a59f50b21123963c0aa7438b5b2ca0a
apartensky <apartensky@gmail.com>
2014-06-04 13:32:51 -0400

    Added complete list of cohort summary fields

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/clinicalSummary/domain/ClinicalSummaryConfig.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/clinicalSummary/domain/ClinicalSummaryConfig.js
@@ -1,6 +1,6 @@
 define([], function(){
 	var configs=[{
-		field: "age_at_initial_pathologic_diagnosis",
+		field: "age_at_initial	_pathologic_diagnosis",
 		dataType: "numeric",
 		chartType: "histogram"
 	},{
@@ -8,7 +8,55 @@ define([], function(){
 		dataType: "numeric",
 		chartType: "histogram"
 	},{
+		field: "days_to_initial_pathologic_diagnosis",
+		dataType: "numeric",
+		chartType: "histogram"
+	},{
+		field: "days_to_last_followup",
+		dataType: "numeric",
+		chartType: "histogram"
+	},{
 		field: "icd_10",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "race",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "history_of_neoadjuvant_treatment",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "person_neoplasm_cancer_status",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "gender",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "histological_type",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "year_of_initial_pathologic_diagnosis",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "vital_status",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "icd_o_3_site",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "ethnicity",
+		dataType: "text",
+		chartType: "piechart"
+	},{
+		field: "tissue_source_site",
 		dataType: "text",
 		chartType: "piechart"
 	}
