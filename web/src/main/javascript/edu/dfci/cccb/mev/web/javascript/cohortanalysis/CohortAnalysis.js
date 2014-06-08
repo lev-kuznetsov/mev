ed8a552269ae8799e7e1de844a4e115a3b1d2436
jfarid27 <jrab227@gmail.com>
2014-06-08 13:37:43 -0400

    Added axis styling and uniform bar color. Fixes #802

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/cohortanalysis/CohortAnalysis.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/cohortanalysis/CohortAnalysis.js
@@ -194,7 +194,7 @@ define(['d3', 'angular', 'colorbrewer/ColorBrewer'], function(d3, angular){
                         	return "translate(" + x(d.x) + "," + y(d.y) + ")";
                         })
                         .attr('fill', function(d,i){
-                        	return d3colors['Set1'][9][(i%9)]
+                        	return d3colors['Set2'][8][0]
                         });
 
                     bar.append("rect")
