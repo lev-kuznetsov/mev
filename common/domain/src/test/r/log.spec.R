2d4aaab4a58f644de578ca370b8741a00d5672b3
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2014-12-14 20:29:06 -0500

    logged test suites

--- common/domain/src/test/r/log.spec.R
+++ common/domain/src/test/r/log.spec.R
@@ -1,1 +1,17 @@
-inject (function (debug) is.function (debug));
+suite ('Logger injectables', function () {
+  it ('Should have injectable debug log',
+      function ()
+        inject (function (debug) expect (is.function (debug))));
+
+  it ('Should have injectable info log',
+      function ()
+        inject (function (info) expect (is.function (info))));
+
+  it ('Should have injectable warn log',
+      function ()
+        inject (function (warn) expect (is.function (warn))));
+
+  it ('Should have injectable error log',
+      function ()
+        inject (function (error) expect (is.function (error))));
+});
