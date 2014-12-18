4e38ea9e4e88195c8f70b2f4bf06cf1f6cd82ed3
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2014-12-18 15:28:26 -0500

    Forgot to reset remote location

--- raven.R
+++ raven.R
@@ -1,3 +1,2 @@
 project ('mev', 'CRYSTAL-SNAPSHOT', modules ('common', 'hclust', 'webapp'));
-#remote ('https://raw.githubusercontent.com/dfci-cccb/maven-repo/master/raven');
-remote ('file:///Users/levk/src/maven-repo/raven');
+remote ('https://raw.githubusercontent.com/dfci-cccb/maven-repo/master/raven');
