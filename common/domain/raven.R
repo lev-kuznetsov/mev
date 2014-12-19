a6623e28641db2268dce435ee5a102d850a1523f
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2014-12-19 11:45:15 -0500

    Switched over to CRAN packaged R loaded via devtools::install_github
    Bumped injectoR

--- common/domain/raven.R
+++ common/domain/raven.R
@@ -1,4 +1,4 @@
 project ('mev-common-domain', 'CRYSTAL-SNAPSHOT', script (sources = file.path ('src', 'main', 'r'),
                                                           tests = file.path ('src', 'test', 'r')));
-dependency ('injectoR', '0.0.3-SNAPSHOT');
+dependency ('injectoR', '0.0.4-SNAPSHOT');
 dependency ('logging', '0.7-103');
