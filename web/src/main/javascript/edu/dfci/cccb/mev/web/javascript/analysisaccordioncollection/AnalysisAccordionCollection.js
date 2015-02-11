83803786fb696eac9da38921e1eecaabd06c0216
jfarid27 <jrab227@gmail.com>
2015-02-11 17:25:47 -0500

    fixed browser bugs

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/analysisaccordioncollection/AnalysisAccordionCollection.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/analysisaccordioncollection/AnalysisAccordionCollection.js
@@ -2,7 +2,7 @@
 
     var deps = ['angular', 'jquery', 'd3', 'alertservice/AlertService'] 
     
-    var directivesDeps = [
+    var directiveDeps = [
       './Directives/analysisContentItem', 
       './Directives/fTestAccordion', 
       './Directives/wilcoxonTestAccordion',
@@ -19,18 +19,18 @@
         './Services/pathService'
     ]
 
-    define(deps.concat(contentDeps).concat(serviceDeps), function(angular, jq, d3){
+    define(deps.concat(directiveDeps).concat(serviceDeps), function(angular, jq, d3){
 	
         var module = angular.module('Mev.AnalysisAccordionCollection', ['Mev.AlertService'])
 
         //load each directive deps file onto the module using function arguments only
-        for (index in directivesDeps){
-            arguments[index + deps.length()](module)
+        for (var index = 0; index < directiveDeps.length; index++){
+            arguments[index + deps.length](module)
         }
         
         //load each service deps file onto module
-        for(index in serviceDeps){
-            arguments[index + deps.length() + directiveDeps.length()](module)
+        for(var index = 0; index < serviceDeps.length; index++){
+            arguments[index + deps.length + directiveDeps.length](module)
         }
         
         return module
