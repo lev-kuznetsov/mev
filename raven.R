4f56813dc4c90e22f5e76c5059cbbad0819b131f
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2015-02-20 17:55:57 -0500

    Limma WIP, remove from raven build for now

--- raven.R
+++ raven.R
@@ -1,2 +1,2 @@
-project ('mev', 'CRYSTAL-SNAPSHOT', modules ('common', 'cluster', 'limma', 'webapp'));
+project ('mev', 'CRYSTAL-SNAPSHOT', modules ('common', 'cluster', 'webapp'));
 remote ('https://raw.githubusercontent.com/dfci-cccb/maven-repo/master/raven');
