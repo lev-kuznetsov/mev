e9b4e4ff0d366904042d9e49533db4e1c8c639d7
apartensky <apartensky@gmail.com>
2015-03-16 11:55:37 -0400

    Format log lines based on log leve (i.e. info/success/error)

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/widgets/analysis/analysisLog/directives/analysisLog.directive.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/widgets/analysis/analysisLog/directives/analysisLog.directive.js
@@ -8,14 +8,21 @@ define(["ng"], function(ng){
 			templateUrl: "app/widgets/analysis/analysisLog/directives/analysisLog.tpl.html",
 			link: function(scope, elem, attrs, ctrl){
 				
+				function newLogline(message, type){
+					return {
+						message: message,
+						type: type
+					};
+				}
+				
 				scope.vm={
-						log: ["initialized"]
+						log: [newLogline("Session log initialized", "info")]
 				};
 				
 				AnalysisEventBus.onAnalysisStarted(scope, function(type, name, eventData){
 					console.debug("onAnalysisStarted handler", type, name, eventData);
 					var message="Started " + type + " analysis " + name;;
-					scope.vm.log.unshift(message);
+					scope.vm.log.unshift(newLogline(message, "info"));
 					if(scope.showAlerts)
 						alertService.info(message,type);
 				});
@@ -23,7 +30,7 @@ define(["ng"], function(ng){
 				AnalysisEventBus.onAnalysisSuccess(scope, function(type, name, data){
 					console.debug("onAnalysisSuccess handler", type, name, data);
 					var message = "Completed " + type + " analysis " + name;
-					scope.vm.log.unshift(message);
+					scope.vm.log.unshift(newLogline(message, "success"));
 					if(scope.showAlerts)
 						alertService.success(message,type);
 				});
@@ -31,9 +38,9 @@ define(["ng"], function(ng){
 				AnalysisEventBus.onAnalysisFailure(scope, function(type, params){
 					console.debug("onAnalysisFailure handler", type, params);
 					var message = "Error while processing " + type + " analysis " + params.name;
-					scope.vm.log.unshift(message);
+					scope.vm.log.unshift(newLogline(message, "error"));
 					if(scope.showAlerts)
-						alertService.error(message,type);
+						alertService.error(message, type);
 				});
 			}
 		};
