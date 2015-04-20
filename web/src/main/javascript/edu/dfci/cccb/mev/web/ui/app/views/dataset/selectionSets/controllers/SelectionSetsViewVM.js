1569145c2fd8f86dfbd86c28600db922f7981b4c
apartensky <apartensky@gmail.com>
2015-04-20 15:08:35 -0400

    Added  instructions on creating a selection set to RowSets and ColumnSets views if no selection sets have been defined yet

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/selectionSets/controllers/SelectionSetsViewVM.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/selectionSets/controllers/SelectionSetsViewVM.js
@@ -4,6 +4,15 @@ define(["ng"], function(ng){
 		this.dataset=dataset;
 		$scope.selections=dataset[dimension].selections;
 		$scope.dataset=dataset[dimension].selections;
+		$scope.vm={
+				addRowSelection: function(){
+					$state.go("root.dataset.rowSet", {datasetId: dataset.id});
+				},
+				addColumnSelection: function(){
+					$state.go("root.dataset.columnSet", {datasetId: dataset.id});
+				}
+				
+		};
 	};
 	SelectionSetsViewVM.$inject=["$scope", "$stateParams", "$state", "dataset", "dimension"];
 	return SelectionSetsViewVM;
