ab8b7690932896309790aad4eba1e04872356a39
apartensky <apartensky@gmail.com>
2015-12-04 10:00:53 -0500

    Pca: performance - return only the first two PC from RServe, otherwise the result size grows O(n^2) in the number of samples

--- pca/domain/src/main/java/edu/dfci/cccb/mev/pca/domain/PcaBuilder.java
+++ pca/domain/src/main/java/edu/dfci/cccb/mev/pca/domain/PcaBuilder.java
@@ -1,16 +1,19 @@
 package edu.dfci.cccb.mev.pca.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import edu.dfci.cccb.mev.dataset.domain.r.AbstractDispatchedRAnalysisBuilder;
 import edu.dfci.cccb.mev.dataset.domain.r.annotation.Callback;
+import edu.dfci.cccb.mev.dataset.domain.r.annotation.Parameter;
 import edu.dfci.cccb.mev.dataset.domain.r.annotation.R;
 import edu.dfci.cccb.mev.dataset.domain.r.annotation.Result;
 
-@R ("function (dataset) {\n"
+@R ("function (dataset, top) {\n"
     + "pca <- prcomp (t (data.matrix (dataset)));\n"
     + "list (sdev = pca$sdev,"
 //    + "      center = as.list (pca$center),"
 //    + "      scale = pca$scale,"
-    + "      x = apply (as.data.frame (t (pca$x)), 2, function (x) as.list (x))"
+    + "      x = apply (as.data.frame (t (pca$x)), 2, function (x) as.list (x)[ 1 : top ])"
 //    + "      rotation = apply (as.data.frame (t (pca$rotation)), 2, function (x) as.list (x))"
     + ");" +
     "}")
@@ -22,6 +25,8 @@ public class PcaBuilder extends AbstractDispatchedRAnalysisBuilder<PcaBuilder, P
 
   private @Result Pca result;
 
+  private @Setter @Getter @Parameter int top = 2;
+  
   @Override
   protected Pca result () {
     return result;
