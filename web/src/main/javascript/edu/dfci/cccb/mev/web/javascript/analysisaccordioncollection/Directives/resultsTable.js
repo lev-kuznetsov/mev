14aa397104a6b7024772233111df49ef2268ab78
apartensky <apartensky@gmail.com>
2015-12-23 16:28:24 -0500

    ResultsTable: fix - number scientific noation with 'Infinity' value

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/analysisaccordioncollection/Directives/resultsTable.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/analysisaccordioncollection/Directives/resultsTable.js
@@ -134,8 +134,8 @@
     	        if (isNaN(input)) {
     	            return input;
     	        } else {
-    	        	if(Math.abs(input)>1000)
-    	        		return input.toExponential(4);
+    	        	if(Math.abs(input)>1000)    	        		 
+    	        		return Number.parseFloat(input).toExponential(fractionSize);
     	        	else
     	        		return $filter('number')(input, fractionSize);
     	        };
