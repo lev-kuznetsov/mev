491fe23310c65e6b0c5fb0783bcafc21d47ec857
apartensky <apartensky@gmail.com>
2016-01-13 16:41:38 -0500

    ui-breadcrumbs: fix import

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/mainmenu/MainMenu.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/mainmenu/MainMenu.module.js
@@ -1,9 +1,9 @@
-define(["angular"], function(angular){	
-	var module=angular.module("Mev.MainMenuModule", []);
-	module.config(["uiBreadcrumbsTemplateProvider", function(uiBreadcrumbsTemplateProvider){
-		console.debug("uiBreadcrumbsTemplateProvider");
-		uiBreadcrumbsTemplateProvider.setPath('/container/vendor/mbAngularUtils/breadcrumbs/origBreadcrumbs.tpl.html');
-	}]);
+define(["angular", "angular-utils-ui-breadcrumbs"], function(angular){	
+	var module=angular.module("Mev.MainMenuModule", ["angularUtils.directives.uiBreadcrumbs"]);
+//	module.config(["uiBreadcrumbsTemplateProvider", function(uiBreadcrumbsTemplateProvider){
+//		console.debug("uiBreadcrumbsTemplateProvider");
+//		uiBreadcrumbsTemplateProvider.setPath('/container/vendor/mbAngularUtils/breadcrumbs/origBreadcrumbs.tpl.html');
+//	}]);
 	module.path="/container/javascript/mainmenu/";
 	return module;
 });
