53ffba4ea4819842f8ede0b6495ea8efafc35f02
apartensky <apartensky@gmail.com>
2016-01-18 01:13:31 -0500

    Steal-tools: fix - blob@0.0.4 requires 'global' to be defined when loading bundle in the browser

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/stealmain.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/stealmain.js
@@ -1,6 +1,8 @@
-define(["ng", "jquery", "app/app", 'orefine/OrefineBridge', './less/boot.less'], function(ng, jquery, app, OpenRefineBridge){		
+//Make sure global is set. blob@0.0.4 expects it to be there.
+window.global = window;
+define(["ng", "jquery", "./app/app", './less/boot.less'], function(ng, jquery, app, OpenRefineBridge){		
 	ng.element(document).ready(function(){
 		ng.bootstrap(document, [app.name]);
 	});
-	window.OpenRefineBridge = OpenRefineBridge;
+	window.OpenRefineBridge = OpenRefineBridge;	
 });
