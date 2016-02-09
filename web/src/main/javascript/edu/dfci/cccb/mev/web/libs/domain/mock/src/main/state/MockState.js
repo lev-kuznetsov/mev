1043bafbbaf9d5d7c7fe8d7d552f8bc9a6cc6d4c
apartensky <apartensky@gmail.com>
2016-02-09 01:12:48 -0500

    mockState: add root and dataset states to mirror mev

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/state/MockState.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/state/MockState.js
@@ -4,9 +4,31 @@ define(["mui", "angular-ui-router", "../project/MockProject"], function(ng){
 	function config($stateProvider){	
 		$stateProvider	   	     		
  		.state("mock", {	 			
+ 			template: "hiiii",
  			resolve:{
  				project: ["mevMockProject", function(mevMockProject){
  					return mevMockProject;
+ 				}]
+ 			}
+ 		})
+ 		.state("root", {
+ 			abstract: true,
+ 			url: "/",
+ 			template: "<ui-view></ui-view>",
+			resolve: {
+ 				project: ["mevMockProject", function(mevMockProject){
+ 					return mevMockProject;
+ 				}]
+ 			}
+ 		})
+ 		.state("root.dataset", {
+ 			parent: "root",
+ 			url: "dataset/{datasetId}/",
+ 			abstract: true,
+ 			template: "<ui-view></ui-view>",
+ 			resolve: {
+ 				dataset: ["project", function(project){
+ 					return project.dataset;
  				}]
  			}
  		});	
