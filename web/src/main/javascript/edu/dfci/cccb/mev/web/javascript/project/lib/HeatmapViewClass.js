1697b70a94db7ad0c7d21993fa8f21b1f7238cb6
apartensky <apartensky@gmail.com>
2016-03-01 12:41:52 -0500

    Heatmap: recalculate min/max/avg when displaying subsets of the original dataset

--- web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/project/lib/HeatmapViewClass.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/javascript/project/lib/HeatmapViewClass.js
@@ -1,12 +1,35 @@
-define(['extend'], function(extend){
+define(['extend', "lodash"], function(extend, _){
 
 	//Constructor :: Object -> $Function [View]
     //  Constructor function for new view that takes Object of type dataset and
     //  optional params object
-	return function(params){
+
+    function getStats(params){
+        var stats = {
+            min: Infinity,
+            max: -Infinity,
+            sum: 0,
+            count: 0
+        };
+        _.transform(params.labels.column.keys, function(result, column, index){
+            _.transform(params.labels.row.keys, function(result, row, index){
+                var value = params.dataset.expression.tryGet([row, column]);
+                if(!isNaN(value)){               
+	                if(value < result.min) result.min = value;
+	                if(value > result.max) result.max = value;
+	                result.sum += value;
+	                result.count++;
+                }
+            }, result);
+        }, stats);          
+        stats.avg = stats.sum / stats.count;
+        return stats;
+    }
+
+	return function(params,dataset){
 
 		var self = this;
-		
+		self.dataset=dataset;
 		this.viewType = 'heatmapView';
 		
 		this.index2labels = function(indexPair){
@@ -15,7 +38,11 @@ define(['extend'], function(extend){
 		
 		this.id = Math.random().toString(36).substring(7);
 		this.applyFilter=function(dimension, keys){
-			self.labels[dimension].keys = keys;
+			if(!_.isEqual(self.labels[dimension].keys, keys)){
+				self.labels[dimension].keys = keys;
+				var stats = getStats(this);
+				_.assign(self.expression, stats);
+			}			
 			return extend({}, self);
 		};
 		
