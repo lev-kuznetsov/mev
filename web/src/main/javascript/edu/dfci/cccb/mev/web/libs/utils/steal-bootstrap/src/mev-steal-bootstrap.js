eb06999c12ec62ae563afe708e75edb4af83d429
apartensky <apartensky@gmail.com>
2016-04-04 14:09:26 -0400

    mev-web: stealmain - use mve-steal-bootstrap module

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/utils/steal-bootstrap/src/mev-steal-bootstrap.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/utils/steal-bootstrap/src/mev-steal-bootstrap.js
@@ -47,14 +47,21 @@
 
   function getConfig(baseURL, main, livePort, live){
     var env = "prod";
+    var relativeBaseURL = baseURL;
+    if(baseURL.charAt(0)==="/"){
+      relativeBaseURL = "";
+      baseURL.match(/\//g).forEach(function(){
+        relativeBaseURL += "../";
+      });
+    }
     var system = {
-      config: baseURL+"package.json!npm",
-      baseURL: baseURL,
+      config: relativeBaseURL+"package.json!npm",
+      baseURL: relativeBaseURL,
       bundlesPath: baseURL+"dist/bundles/min",
       main: main,
       loadBundles: true,
       getStealSrc: function getStealSrc(){      
-        var stealsrc = baseURL+"/node_modules/steal/steal.js";          
+        var stealsrc = baseURL+"node_modules/steal/steal.js";          
         if(this && this.loadBundles)
           stealsrc = this.bundlesPath+"/"+this.main+".js"      
         return stealsrc;
@@ -99,6 +106,8 @@
     stealscript.setAttribute('charset',"UTF-8");
     document.head.appendChild(stealscript);
   }
+  global.getConfig = getConfig;
+  global.start = startSteal;
   global.live=global.startStealLive;
   global.writeCookie = writeCookie;
   global.readCookie = readCookie;
