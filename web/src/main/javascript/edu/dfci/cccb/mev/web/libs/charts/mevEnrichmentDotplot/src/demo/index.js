65b0b87b4f024d108706a176309a46a5cde19f6f
apartensky <apartensky@gmail.com>
2016-04-12 11:58:37 -0400

    add mevEnrichmentDotplot

---
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevEnrichmentDotplot/src/demo/index.js
@@ -0,0 +1,13 @@
+define(["mui", "mev-enrichment-dotplot", "./topgo_dummy.json"], function(ng, mod, topgoJson){
+	var app = ng.module("mev-enrichment-dotplot-demo", arguments, arguments);
+	
+	app.controller('MainCtrl', ["$scope", "mevEnrichmentDataAdaptor", function($scope, mevEnrichmentDataAdaptor) {
+		$scope.config={
+			data: mevEnrichmentDataAdaptor(topgoJson.results),
+			series: "Counts"
+		};
+	}]);
+	ng.element(document).ready(function(){
+		ng.bootstrap(document, [app.name]);
+	});	
+});
