5c75fa0b57744d4e7dcbb9d59fddec97b7c6ca0f
apartensky <apartensky@gmail.com>
2016-04-18 16:58:44 -0400

    Shell script to unlink src folders from target when in dev mode

---
+++ web/udevlink.sh
@@ -0,0 +1,7 @@
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+rm $BASEDIR/target/classes/edu/dfci/cccb/mev/web/ui
+rm $BASEDIR/target/classes/edu/dfci/cccb/mev/web/libs
+rm $BASEDIR/target/classes/edu/dfci/cccb/mev/web/mock
+rm $BASEDIR/target/classes/edu/dfci/cccb/mev/web/vendor
+rm $BASEDIR/target/classes/edu/dfci/cccb/mev/web/javascript
