759b783622715feb9342c363b002cb38ca3b9255
apartensky <apartensky@gmail.com>
2016-04-27 13:52:45 -0400

    mevHBarchart: fefactor to use mevChartUtils:mevTooltipContent

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevHBarchart/src/main/view/mevHBarchartDirective.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevHBarchart/src/main/view/mevHBarchartDirective.js
@@ -6,7 +6,7 @@ define(["lodash", "./mevHBarchart.tpl.html"], function(_, template){"use strict"
 				config: "=mevHbarchart"
 			},
 			template: template,
-			controller: ["$scope", "mevBarchartNvd3Adaptor", function(scope, mevBarchartNvd3Adaptor){
+			controller: ["$scope", "mevBarchartNvd3Adaptor", "mevTooltipContent", function(scope, mevBarchartNvd3Adaptor, mevTooltipContent){
 
 				function mixinGetter(dimConfig){
 					if(_.isString(dimConfig.field)){
@@ -67,89 +67,8 @@ define(["lodash", "./mevHBarchart.tpl.html"], function(_, template){"use strict"
 		                },
 		                margin: {"left": 400},
 		                tooltip: {
-			                	contentGenerator: function(item) {
-			                		// Format function for the tooltip values column.
-								    var valueFormatter = function(d, i) {
-								        return d;
-								    };
-
-								    // Format function for the tooltip header value.
-								    var headerFormatter = function(d) {
-								        return "" + ": " + d;
-								    };
-
-								    var keyFormatter = function(d, i) {
-								        return d;
-								    };
-
-							        if (item === null) {
-							            return '';
-							        }
-
-							        var table = d3.select(document.createElement("table"));
-						            var theadEnter = table.selectAll("thead")
-						                .data([item])
-						                .enter().append("thead");
-
-						            theadEnter.append("tr")
-						                .append("td")
-						                .attr("colspan", 3)
-						                .append("strong")
-						                .classed("id", true)
-						                .html(zConfig.label + ": " + zConfig.get(item.data));
-
-						            
-						        
-
-							        var tbodyEnter = table.selectAll("tbody")
-							            .data([item])
-							            .enter().append("tbody");
-
-							        var trowEnter = tbodyEnter.selectAll("tr")
-							                .data(function(p) { return p.series;})
-							                .enter()
-							                .append("tr")
-							                .classed("highlight", function(p) { return p.highlight;});
-
-							        trowEnter.append("td")
-							            .classed("legend-color-guide",true)
-							            .append("div")
-							            .style("background-color", function(p) { return p.color;});
-
-							        trowEnter.append("td")
-							            .classed("key",true)
-							            .classed("total",function(p) { return !!p.total;})
-							            .html(function(p, i) { return keyFormatter(p.key, i);});
-
-							        trowEnter.append("td")
-							            .classed("value",true)
-							            .html(function(p, i) {
-											var fields = scope.config.tooltip ? scope.config.tooltip.fields : {};
-											return _.reduce(fields,
-												function(html, field, key){
-													return html += ", " + key
-														+ ": " +( _.isFunction(field) ? field.call(item.data, item.data) : item.data[field]);
-												},
-												yConfig.get(item.data));
-										});
-
-							        trowEnter.selectAll("td").each(function(p) {
-							            if (p.highlight) {
-							                var opacityScale = d3.scale.linear().domain([0,1]).range(["#fff",p.color]);
-							                var opacity = 0.6;
-							                d3.select(this)
-							                    .style("border-bottom-color", opacityScale(opacity))
-							                    .style("border-top-color", opacityScale(opacity))
-							                ;
-							            }
-							        });
-
-							        var html = table.node().outerHTML;
-							        if (item.footer !== undefined)
-							            html += "<div class='footer'>" + d.footer + "</div>";
-							        return html;
-							    }
-			                },
+							contentGenerator: mevTooltipContent.bind(null, scope.config)
+						},
 		            }
 		        };
 			}],
@@ -193,6 +112,6 @@ define(["lodash", "./mevHBarchart.tpl.html"], function(_, template){"use strict"
 	};
 	directive.$name="mevHbarchart";
 	directive.$provider="directive";
-	directive.$inject=["mevBarchartNvd3Adaptor"];
+	directive.$inject=["mevBarchartNvd3Adaptor", "mevTooltipContent"];
 	return directive;
 });
