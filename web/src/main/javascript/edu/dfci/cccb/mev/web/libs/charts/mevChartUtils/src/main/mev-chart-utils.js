4c9cdf635dd332ec3d30a19ef4e39e80627f415b
apartensky <apartensky@gmail.com>
2016-04-27 14:16:39 -0400

    mevChartUtils: add model for mevChartDimConfig

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevChartUtils/src/main/mev-chart-utils.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevChartUtils/src/main/mev-chart-utils.js
@@ -1,3 +1,6 @@
-define(["mui", "./services/mevTooltipContent", "./style/mevTooltipContent.less"], function(ng){
+define(["mui",
+   "./services/mevTooltipContent",
+    "./model/mevChartDimConfig",
+   "./style/mevTooltipContent.less"], function(ng){
    return ng.module("mev-chart-utils", arguments, arguments);
 });
