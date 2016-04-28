0d79b95cfee91755702c7d5d18ec9c7fcd271fc8
apartensky <apartensky@gmail.com>
2016-04-28 13:26:48 -0400

    mevColorLegend: add ability to change colors

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevChartUtils/src/main/model/mevChartColorDimConfig.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevChartUtils/src/main/model/mevChartColorDimConfig.js
@@ -1,11 +1,17 @@
 define(["lodash"], function(_){
-    function mixinColorDimConfig(colorConfig, data){
+    function mixinColorDimConfig(colorConfig, data, onUpdateColor){
         if(!colorConfig)
             throw new Error("colorConfig is undefined " + JSON.stringify(colorConfig));
         _.extend(colorConfig, {
             colors: ["blue", "yellow"],
             min: 0,
-            max: colorConfig.get(_.maxBy(data, colorConfig.get))
+            max: colorConfig.get(_.maxBy(data, colorConfig.get)),
+            updateColor: function(color){
+                this.colors = color.range;
+                this.scale = d3.scale.linear().domain([colorConfig.min, colorConfig.max]).range(colorConfig.colors)
+                if(onUpdateColor)
+                    onUpdateColor();
+            }
         });
         _.extend(colorConfig, {
             scale: d3.scale.linear().domain([colorConfig.min, colorConfig.max]).range(colorConfig.colors)
@@ -16,7 +22,7 @@ define(["lodash"], function(_){
     var factory = function(){
         return mixinColorDimConfig;
     };
-    factory.$name="mevChartColorDimConfig";
+    factory.$name="mevChartColorDimConfig";32
     factory.$provider="factory";
     factory.$inject=[];
     return factory;
--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevChartUtils/src/main/view/colorLegend/mevChartColorLegendDirective.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevChartUtils/src/main/view/colorLegend/mevChartColorLegendDirective.js
@@ -1,49 +1,93 @@
-define(["lodash", "d3", "./mevChartColorLegend.less"], function(_, d3){
+define(["lodash", "d3", "./mevChartColorLegend.tpl.html", "./mevChartColorLegend.less"], function(_, d3, template){
    var directive = function(){
        return {
            restrict: "AEC",
            scope: {
             config: "=?mevChartColorLegend"
            },
-           link: function(scope, elem, attr, ctrl){
+           template: template,
+           controller: ["$scope", function(scope){
                if(!scope.config)
                    throw new Error("Config is undefined");
 
                _.extend(scope.config, {
                    legend: {
-                       width: 130,
-                       height: 250
+                       width: 80,
+                       height: 190,
+                       margin: [30, 50],
+                       color: {
+                           available: [
+                               {
+                                   label: "Blue,Yellow",
+                                   range: ["blue", "yellow"]
+                               },
+                               {
+                                   label: "Red,Green",
+                                   range: ['red', 'green'],
+                               },
+                               {
+                                   label: "Red,Blue",
+                                   range: ['red', 'blue']
+                               }
+                           ],
+                           find: function(color){
+                               if(_.isArray(color))
+                                   return _.find(this.available, function(item){
+                                       return _.isEqual(color, item.range);
+                                   });
+                               else
+                                   return _.find(this.available, function(item){
+                                       return color===item.label;
+                                   });
+                           }
+                       }
                    }
-                });
+               });
 
-               var colorLegendConfig = scope.config.legend;
-               var d3root = d3.select(elem[0]);
-               d3root.select("svg").remove();
-               var svgColorValueLegend = d3root.append("svg");
-               svgColorValueLegend.attr("width", colorLegendConfig.width).attr("height", colorLegendConfig.height);
 
-               var w = colorLegendConfig.width, h = colorLegendConfig.height;
-               var key = svgColorValueLegend;
-               var legend = key.append("defs").append("svg:linearGradient")
-                   .attr("id", "gradient")
-                   .attr("x1", "100%")
-                   .attr("y1", "0%")
-                   .attr("x2", "100%")
-                   .attr("y2", "100%")
-                   .attr("spreadMethod", "pad");
+           }],
+           link: function(scope, elem, attr, ctrl){
+               scope.vm = {
+                   updateColor: function updateColor($event){
+                       console.debug("udpatecolor", scope.vm.color);
+                       scope.config.updateColor(scope.vm.color);
+                       this.draw(scope.config, d3.select(elem[0]));
+                   },
+                   color: scope.config.legend.color.find(scope.config.colors),
+                   draw: function(config, d3root){
+                       var colorLegendConfig = config.legend;
+                       d3root.select("svg").remove();
+                       var svgColorValueLegend = d3root.insert("svg", ":first-child");
+                       svgColorValueLegend.attr("width", colorLegendConfig.width).attr("height", colorLegendConfig.height);
 
-               legend.append("stop").attr("offset", "0%").attr("stop-color", "yellow").attr("stop-opacity", 1);
-               legend.append("stop").attr("offset", "100%").attr("stop-color", "blue").attr("stop-opacity", 1);
+                       var w = colorLegendConfig.width, h = colorLegendConfig.height;
+                       var marginv = _.isArray(colorLegendConfig.margin) ? colorLegendConfig.margin[0] : colorLegendConfig.margin;
+                       var marginh = _.isArray(colorLegendConfig.margin) ? colorLegendConfig.margin[1] : colorLegendConfig.margin;
+                       var key = svgColorValueLegend;
+                       var legend = key.append("defs").append("svg:linearGradient")
+                           .attr("id", "gradient")
+                           .attr("x1", "100%")
+                           .attr("y1", "0%")
+                           .attr("x2", "100%")
+                           .attr("y2", "100%")
+                           .attr("spreadMethod", "pad");
 
-               key.append("rect").attr("width", w - 100).attr("height", h - 100).style("fill", "url(#gradient)").attr("transform", "translate(0,10)");
+                       legend.append("stop").attr("offset", "0%").attr("stop-color", this.color.range[1]).attr("stop-opacity", 1);
+                       legend.append("stop").attr("offset", "100%").attr("stop-color", this.color.range[0]).attr("stop-opacity", 1);
 
-               var y = d3.scale.linear().range([h - 100, 0]).domain([scope.config.min, scope.config.max]).nice();
+                       key.append("rect").attr("width", w - marginh).attr("height", h - marginv).style("fill", "url(#gradient)").attr("transform", "translate(0,10)");
 
-               var yAxis = d3.svg.axis().scale(y).orient("right");
+                       var y = d3.scale.linear().range([h - marginv, 0]).domain([config.min, config.max]).nice();
 
-               key.append("g").attr("class", "y axis").attr("transform", "translate(31,10)")
-                   .call(yAxis)
-                   .append("text").attr("y", h-100).attr("dy", ".71em").style("text-anchor", "end").text(scope.config.label);
+                       var yAxis = d3.svg.axis().scale(y).orient("right");
+
+                       key.append("g").attr("class", "y axis").attr("transform", "translate(31,10)")
+                           .call(yAxis)
+                           .append("text").attr("y", h - marginv).attr("dy", ".71em").style("text-anchor", "end").text(config.label);
+                   }
+               }
+
+               scope.vm.draw(scope.config, d3.select(elem[0]));
            }
        }
    };
