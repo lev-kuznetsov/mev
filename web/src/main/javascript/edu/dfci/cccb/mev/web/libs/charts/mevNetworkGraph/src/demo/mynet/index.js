b39554e72f0cfdf98ff042ab64f27eda67d027dd
apartensky <apartensky@gmail.com>
2016-05-05 11:51:44 -0400

    mevNetworGraph: tooltip with dynamic fields in vega

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevNetworkGraph/src/demo/mynet/index.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/charts/mevNetworkGraph/src/demo/mynet/index.js
@@ -3,6 +3,11 @@ function(ng, d3, vg, networkJson){
     var demo = ng.module("mev-network-graph-demo", arguments, arguments)
         .controller("DemoCtrl", ["$scope", function($scope){
             $scope.network = networkJson;
+            $scope.data = {
+                "tooltipFields": [{"field": "name"},
+                    {"field": "index" },
+                    {"field": "group" }]
+            }
         }]);
     ng.element(document).ready(function(){
         ng.bootstrap(document, [demo.name]);
