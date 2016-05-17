451a3a6830261ebd1d187ce2e28ab8e64e216994
apartensky <apartensky@gmail.com>
2016-05-16 23:57:38 -0400

    AnalysisController: DELETE method

--- dataset/rest/src/main/java/edu/dfci/cccb/mev/dataset/rest/controllers/AnalysisController.java
+++ dataset/rest/src/main/java/edu/dfci/cccb/mev/dataset/rest/controllers/AnalysisController.java
@@ -16,19 +16,21 @@ package edu.dfci.cccb.mev.dataset.rest.controllers;
 
 import static edu.dfci.cccb.mev.dataset.rest.resolvers.AnalysisPathVariableMethodArgumentResolver.ANALYSIS_URL_ELEMENT;
 import static edu.dfci.cccb.mev.dataset.rest.resolvers.DatasetPathVariableMethodArgumentResolver.DATASET_URL_ELEMENT;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST;
 
 import javax.inject.Inject;
 
+import edu.dfci.cccb.mev.dataset.domain.contract.AnalysisNotFoundException;
+import edu.dfci.cccb.mev.dataset.domain.contract.Dataset;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import edu.dfci.cccb.mev.dataset.domain.contract.Analysis;
 
@@ -42,7 +44,8 @@ import edu.dfci.cccb.mev.dataset.domain.contract.Analysis;
 @Scope (SCOPE_REQUEST)
 public class AnalysisController {
 
-  private @Getter @Setter (onMethod = @_ (@Inject)) Analysis analysis;
+  private @Inject Dataset dataset;
+  private @Inject Analysis analysis;
 
   @RequestMapping (method = GET)
   @ResponseBody
@@ -50,5 +53,12 @@ public class AnalysisController {
     if (log.isDebugEnabled ())
       log.debug ("Returning analysis " + analysis);
     return analysis;
+  }
+
+  @RequestMapping (method = DELETE)
+  @ResponseStatus(HttpStatus.OK)
+  public void deleteByName (@PathVariable("analysis") String name) throws AnalysisNotFoundException {
+    log.info ("Deleting analysis " + name);
+    dataset.analyses().remove(name);
   }
 }
