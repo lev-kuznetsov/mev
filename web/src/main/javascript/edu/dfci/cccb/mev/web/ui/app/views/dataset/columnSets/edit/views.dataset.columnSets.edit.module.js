4e308d2bb383a073932246717712bbbe4bb28e57
apartensky <apartensky@gmail.com>
2016-05-17 18:16:53 -0400

    ColumnSets: use the common resolvers and handlers defined for SelectionSets

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/columnSets/edit/views.dataset.columnSets.edit.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/dataset/columnSets/edit/views.dataset.columnSets.edit.module.js
@@ -1,13 +1,13 @@
-define(["mui", "lodash", "angular-color-picker",
-	"./views.dataset.columnSets.edit.tpl.html", "angular-color-picker/angular-color-picker.css"],
+define(["mui", "lodash", 
+	"../../selectionSets/views.dataset.selectionSets.module"],
 
 function(ng, _, ngColorPicker,
-		 template, colorPickerCss){
+		 template){
 	
 	var module=ng.module("mui.views.dataset.columnSets.edit", arguments, arguments);
 	
-	module.config(['$stateProvider',
-		function($stateProvider) {
+	module.config(['$stateProvider', 'SelectionSetResolverProvider', 'SelectionSetsEditOnEnterProvider',
+		function($stateProvider, SelectionSetResolverProvider, SelectionSetsEditOnEnterProvider) {
 
 			$stateProvider.state("root.dataset.columnSets.edit", {
 					url: "/edit/{name}",
@@ -16,54 +16,9 @@ function(ng, _, ngColorPicker,
 						setId: undefined
 					},
 					resolve: {
-						selectionSet: ["$stateParams", "dataset", function($stateParams, dataset){
-							var reset = dataset.resetSelections("column");
-							console.debug("resolve ColumnSet", reset);
-
-							return reset.$promise.then(function(){
-								var columnSet = _.find(dataset.column.selections, function(selection){
-									return selection.name === $stateParams.setId;
-								});
-
-								if(!columnSet){
-									throw new Error("Selection Set '" + $stateParams.setId + "' not found" );
-								}
-
-								console.debug("resolved ColumnSet", columnSet);
-								return columnSet;
-							});
-						}]
+						selectionSet: SelectionSetResolverProvider.$get()
 					},
-					onEnter: ["$stateParams", "$state", "$uibModal", "selectionSet", "dataset", "dimension",
-						function($stateParams, $state, $modal, selectionSet, dataset, dimension){
-						$modal.open({
-							template: template,
-							resolve: {
-								selectionSet: ["$stateParams", function ($stateParams) {
-									return selectionSet;
-								}],
-							},
-							controller: ["$scope", "selectionSet", function(scope, selectionSet){
-								scope.selectionSet = _.cloneDeep(selectionSet);
-								scope.dismiss = function() {
-									scope.$dismiss();
-								};
-								scope.save = function() {
-									_.assign(selectionSet, scope.selectionSet);
-									dataset.selection.put({
-										datasetName: dataset.datasetName,
-										dimension: dimension
-									}, selectionSet,
-									function (response) {
-										dataset.resetSelections(dimension);
-									});
-									scope.$close(true);
-								};
-							}]
-						}).result.finally(function(){
-							$state.go("^");
-						});
-					}]
+					onEnter: SelectionSetsEditOnEnterProvider.$get()
 				});
 		}]);
 	return module;
