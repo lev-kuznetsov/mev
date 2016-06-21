21eec0de228104096205dfb5267db75edda31add
apartensky <apartensky@gmail.com>
2016-06-21 14:02:08 -0400

    mevWorkspace: add upload dataset directive

---
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/workspace/src/main/view/upload/UploadDatasetDirective.js
@@ -0,0 +1,36 @@
+define(["./UploadDataset.tpl.html"], function(template){
+    var directive = function(DatasetResource){
+
+        return {
+            restrict : 'CEA',
+            template : template,
+            link : function(scope, elem, attrs) {
+                var elButton = elem.find(".btn");
+                var elInput = elem.find("input");
+                elButton.click(function() {
+                    elInput.click();
+                });
+
+                elInput.on(
+                    "change",
+                    function() {
+                        var input = elInput[0],
+                            files = new Array();
+                        for ( var i = 0; i < input.files.length; i++) {
+                            files.push(input.files[i]);
+                            if (files.length == input.files.length) {
+                                files.map(function(file){
+                                    DatasetResource.uploadFile(file);
+                                });
+                            }
+                        }
+                    });
+            }
+        };
+    };
+    directive.$name="mevDatasetUpload";
+    directive.$inject=["DatasetResourceService"];
+    directive.$provider="directive";
+    return directive;
+});
+
