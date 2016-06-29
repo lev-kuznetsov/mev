5d9b371030a5611a27ede67897a038f9cc5f0c00
apartensky <apartensky@gmail.com>
2016-06-29 16:52:05 -0400

    Datasets State: move import tabs (tcga, geo, google) each to its own state; add tutorials tab

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/datasets/views.datasets.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/datasets/views.datasets.module.js
@@ -1,19 +1,55 @@
 define(["mui",
 	"./_controllers/DatasetsVM",
+	"./_templates/views.datasets.tutorials.tpl.html",
+	"./_templates/views.datasets.google.tpl.html",
 	"../../widgets/presets/widgets.presets.module",
 	"./session/views.datasets.session.module"],
-function(ng, DatasetsVM){
+function(ng, DatasetsVM, tutorialsTemplate, googleTemplate){
 	var module = ng.module("mui.views.datasets", arguments, arguments);
 	module.config(['$stateProvider', '$urlRouterProvider',
-	     	function($stateProvider, $urlRouterProvider){	     						
-	     		$stateProvider.state("root.datasets", {
-	     			url: "/datasets",	     			
-	     			controller: "ImportsCtrl",
-	     			controllerAs: "ImportsCtrl",
-	     			parent: "root",
-	     			templateUrl: "app/views/datasets/_templates/views.datasets.tpl.html",
-	     			displayName: "datasets"
-	     		});
+	     	function($stateProvider, $urlRouterProvider){
+				$urlRouterProvider.when("/datasets", "/datasets/tutorials");
+	     		$stateProvider
+					.state("root.datasetsOld", {
+						url: "/datasetsOld",
+						controller: "ImportsCtrl",
+						controllerAs: "ImportsCtrl",
+						parent: "root",
+						templateUrl: "app/views/datasets/_templates/views.datasets.tpl.html",
+						displayName: "datasets"
+					})
+					.state("root.datasets", {
+						url: "/datasets",
+						controller: "ImportsCtrl",
+						controllerAs: "ImportsCtrl",
+						parent: "root",
+						templateUrl: "app/views/datasets/_templates/views.datasets2.tpl.html",
+						displayName: "datasets"
+					})
+					.state("root.datasets.tutorials", {
+						url: "/tutorials",
+						parent: "root.datasets",
+						template: tutorialsTemplate,
+						displayName: "tutorials"
+					})
+					.state("root.datasets.tcga", {
+						url: "/tcga",
+						parent: "root.datasets",
+						template: "<div id=\"presetMgr\" presets-list></div>",
+						displayName: "TCGA"
+					})
+					.state("root.datasets.geods", {
+						url: "/geods",
+						parent: "root.datasets",
+						template: "<div class='row'><div class='col-md-12' id=\"geodsImportMgr\" mev-geods-import-directive></div></div>",
+						displayName: "GEO"
+					})
+					.state("root.datasets.google", {
+						url: "/google",
+						parent: "root.datasets",
+						template: googleTemplate,
+						displayName: "Google Drive"
+					});;
 	}]);
 	// module.controller("DatasetsVM", DatasetsVM);
 	return module;
