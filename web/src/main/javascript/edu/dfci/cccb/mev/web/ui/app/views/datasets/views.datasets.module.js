e14cd967918ed01743f2f6db1f271eb3f22e515d
apartensky <apartensky@gmail.com>
2016-07-15 16:26:38 -0400

    Datasets: change Datasets state to use two views - one for workspace, one for import tabs

--- web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/datasets/views.datasets.module.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/ui/app/views/datasets/views.datasets.module.js
@@ -2,13 +2,14 @@ define(["mui",
 	"./_controllers/DatasetsVM",
 	"./_templates/views.datasets.tutorials.tpl.html",
 	"./_templates/views.datasets.google.tpl.html",
+	"./_templates/views.datasets.imports.tpl.html",
 	"../../widgets/presets/widgets.presets.module",
 	"./session/views.datasets.session.module"],
-function(ng, DatasetsVM, tutorialsTemplate, googleTemplate){
+function(ng, DatasetsVM, tutorialsTemplate, googleTemplate, importsTemplate){
 	var module = ng.module("mui.views.datasets", arguments, arguments);
 	module.config(['$stateProvider', '$urlRouterProvider',
 	     	function($stateProvider, $urlRouterProvider){
-				$urlRouterProvider.when("/datasets", "/datasets/tcga");
+				$urlRouterProvider.when("/datasets", "/datasets/workspace");
 	     		$stateProvider
 					.state("root.datasetsOld", {
 						url: "/datasetsOld",
@@ -20,36 +21,51 @@ function(ng, DatasetsVM, tutorialsTemplate, googleTemplate){
 					})
 					.state("root.datasets", {
 						url: "/datasets",
-						controller: "ImportsCtrl",
-						controllerAs: "ImportsCtrl",
 						parent: "root",
+						displayName: "datasets",
 						templateUrl: "app/views/datasets/_templates/views.datasets2.tpl.html",
-						displayName: "datasets"
+						// deepStateRedirect: {
+						// 	default: {
+						// 		state: "root.datasets.workspace"
+						// 	}
+						// }
+						redirectTo: "root.datasets.imports.tcga"
 					})
-					.state("root.datasets.tutorials", {
-						url: "/tutorials",
+					.state("root.datasets.imports", {
 						parent: "root.datasets",
+						displayName: false,
+						views: {
+							"imports": {
+								template: "<div ui-view></div>"
+							}
+						},
+						sticky: true,
+						
+					})
+					.state("root.datasets.imports.tutorials", {
+						url: "/tutorials",
+						parent: "root.datasets.imports",
 						template: tutorialsTemplate,
 						displayName: "tutorials"
 					})
-					.state("root.datasets.tcga", {
+					.state("root.datasets.imports.tcga", {
 						url: "/tcga",
-						parent: "root.datasets",
+						parent: "root.datasets.imports",
 						template: "<div id=\"presetMgr\" presets-list></div>",
 						displayName: "TCGA"
 					})
-					.state("root.datasets.geods", {
+					.state("root.datasets.imports.geods", {
 						url: "/geods",
-						parent: "root.datasets",
+						parent: "root.datasets.imports",
 						template: "<div class='row'><div class='col-md-12' id=\"geodsImportMgr\" mev-geods-import-directive></div></div>",
 						displayName: "GEO"
 					})
-					.state("root.datasets.google", {
+					.state("root.datasets.imports.google", {
 						url: "/google",
-						parent: "root.datasets",
+						parent: "root.datasets.imports",
 						template: googleTemplate,
 						displayName: "Google Drive"
-					});;
+					});
 	}]);
 	// module.controller("DatasetsVM", DatasetsVM);
 	return module;
