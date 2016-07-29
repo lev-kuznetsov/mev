4108c3fa7a4baf4c1e8cfafb0cb328e98614759b
apartensky <apartensky@gmail.com>
2016-07-29 15:20:57 -0400

    MockProject: allow mocking of multiple datasets

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/project/MockProject.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/project/MockProject.js
@@ -1,11 +1,19 @@
-define(["mev-project/data/mouse_test_data.tsv.json", "mev-project"], 
-function(mouseJson){ "use strict";
-	function value(mevProject){		
-		var project = mevProject("mouse_test_data.tsv", mouseJson);
-		return project;
+define(["mev-project/data/mouse_test_data.tsv.json", "mev-mock/data/deseq_test_data.tsv.json", "mev-project"],
+function(mouseJson, deseqJson){ "use strict";
+	var service = function(){
+		this.datasetName = "mouse_test_data.tsv";
+		this.datasets = {
+			"mouse_test_data.tsv": mouseJson,
+			"deseq_test_data.tsv": deseqJson
+		}
+		this.$get = ["mevProject", function service(mevProject){
+			var project = mevProject(this.datasetName, this.datasets[this.datasetName]);
+			return project;
+		}];
 	}
-	value.$inject=["mevProject"];
-	value.$name="mevMockProject";
-	value.$provider="service";
-	return value;
+
+	service.$inject=[];
+	service.$name="mevMockProject";
+	service.$provider="provider";
+	return service;
 });
