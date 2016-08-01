1935938921203f323e037eb555b60180393578d7
apartensky <apartensky@gmail.com>
2016-08-01 18:15:46 -0400

    mevMock: add lgg mock

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/project/MockProject.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/project/MockProject.js
@@ -1,10 +1,14 @@
-define(["mev-project/data/mouse_test_data.tsv.json", "mev-mock/data/deseq_test_data.tsv.json", "mev-project"],
-function(mouseJson, deseqJson){ "use strict";
+define(["mev-project/data/mouse_test_data.tsv.json",
+	"mev-mock/data/deseq_test_data.tsv.json",
+	"mev-mock/data/lgg.json",
+	"mev-project"],
+function(mouseJson, deseqJson, lggJson){ "use strict";
 	var service = function(){
 		this.datasetName = "mouse_test_data.tsv";
 		this.datasets = {
 			"mouse_test_data.tsv": mouseJson,
-			"deseq_test_data.tsv": deseqJson
+			"deseq_test_data.tsv": deseqJson,
+			"lgg": lggJson
 		}
 		this.$get = ["mevProject", function service(mevProject){
 			var project = mevProject(this.datasetName, this.datasets[this.datasetName]);
