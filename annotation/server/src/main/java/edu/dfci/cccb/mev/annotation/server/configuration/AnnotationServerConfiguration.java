d704d306872a4851c5354f72ac8062b06d502dd9
apartensky <apartensky@gmail.com>
2016-08-10 01:11:33 -0400

    AnnotationServerConfiguration: factor out ProjectManager bean into its own configuratin - this config is does not load AnnotationController which was causing issues with parallel tests due to Butterfly servlet being a singleton

--- annotation/server/src/main/java/edu/dfci/cccb/mev/annotation/server/configuration/AnnotationServerConfiguration.java
+++ annotation/server/src/main/java/edu/dfci/cccb/mev/annotation/server/configuration/AnnotationServerConfiguration.java
@@ -11,12 +11,8 @@ import javax.inject.Named;
 
 import lombok.ToString;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,34 +33,7 @@ import edu.dfci.cccb.mev.presets.contract.PresetDimensionBuilder;
                                                                       Controller.class,
                                                                       ControllerAdvice.class,
                                                                       RestController.class }))
+@Import({AnnotationProjectManagerConfiguration.class})
 @ToString
 public class AnnotationServerConfiguration extends WebMvcConfigurerAdapter {
-
-  private @Inject Workspace workspace;
-  private @Inject FileProjectManager sessionProjectManager;
-  private @Inject @Named("presets-dataset-builder") PresetDatasetBuilder builder;  
-  private @Inject org.springframework.core.env.Environment environment;
-  private @Inject ProbeAnnotationPlatforms annotationPlatforms;
-  private @Inject PresetDimensionBuilder dimensionBuilder;
-  
-  @Bean
-  @Scope (value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
-  public FileProjectManager sessionProjectManager () throws IOException, SQLException {
-    FileProjectManager projectManager = new FileProjectManager ();
-    projectManager.setWorkspaceDir (new SessionWorkspaceDir ());
-    projectManager.setWorkspace (workspace);
-    projectManager.setDatasetBuilder (builder);
-    projectManager.setDimensionBuilder (dimensionBuilder);
-    projectManager.setProbeAnnotationPlatforms (annotationPlatforms);
-    return projectManager;
-  }
-
-
-  @PostConstruct
-  public void setProjectmanagerSingleton () {
-    ProjectManager.setSingleton (sessionProjectManager);
-  }
-  
-  
-
 }
