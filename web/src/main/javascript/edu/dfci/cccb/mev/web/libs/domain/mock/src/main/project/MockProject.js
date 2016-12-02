4c60f11af5766f72fbd237eb3fa537327771a13b
Antony Partensky <apartensky@gmail.com>
2016-12-02 18:56:10 -0500

    mevMockProject: add dummy json for raw_count_matrix.rand.primary.counts.1000.tsv.json dataset to be used with EdgeR analysis demo

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/project/MockProject.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/mock/src/main/project/MockProject.js
@@ -1,14 +1,16 @@
 define(["mev-project/data/mouse_test_data.tsv.json",
 	"mev-mock/data/deseq_test_data.tsv.json",
 	"mev-mock/data/lgg.json",
+	"mev-mock/data/raw_count_matrix.rand.primary.counts.1000.tsv.json",
 	"mev-project"],
-function(mouseJson, deseqJson, lggJson){ "use strict";
+function(mouseJson, deseqJson, lggJson, rawCountJson){ "use strict";
 	var service = function(){
 		this.datasetName = "mouse_test_data.tsv";
 		this.datasets = {
 			"mouse_test_data.tsv": mouseJson,
 			"deseq_test_data.tsv": deseqJson,
-			"lgg": lggJson
+			"lgg": lggJson,
+			"raw_count_matrix.rand.primary.counts.1000.tsv": rawCountJson
 		}
 		this.$get = ["mevProject", function service(mevProject){
 			var project = mevProject(this.datasetName, this.datasets[this.datasetName]);
