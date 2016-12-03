060211191065b09e7aca3f0be198662c944c18d0
Antony Partensky <apartensky@gmail.com>
2016-12-02 19:02:24 -0500

    EdgeR: demo - configure mevMockProject to use the raw_count_matrix.rand.primary.counts.1000.tsv

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/analyses/edger/src/demo/index.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/analyses/edger/src/demo/index.js
@@ -4,6 +4,9 @@ define(["mui", "mev-edger", "./edger_mock.json",
     "mev-mock",
     "bootstrap", "bootstrap/dist/css/bootstrap.min.css"], function(ng, mevEdger, edgerJson, AnalysisClass){"use strict";
     var demo = ng.module("mev-edger-demo", arguments, arguments)
+    .config(["mevMockProjectProvider", function(mevMockProjectProvider){
+        mevMockProjectProvider.datasetName = "raw_count_matrix.rand.primary.counts.1000.tsv";
+    }])
     .controller("demoCtrl", ["$scope", "mevEdgerAnalysisType", "mevAnalysisTypes", function(scope, mevEdgerAnalysisType, mevAnalysisTypes){
         scope.analysisType = mevEdgerAnalysisType;
         scope.analysisTypes = mevAnalysisTypes;
