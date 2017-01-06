00f1d6e65eb13b6c786e98ec3094911854d890ef
Antony Partensky <apartensky@gmail.com>
2017-01-06 17:04:48 -0500

    SelectinSetParam: add disjoint attribute - ensures that this selectionSet is disjoint with anotehr selectionSet parameter

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/analysis/src/params/model/selectionSet/SelectionSetParam.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/analysis/src/params/model/selectionSet/SelectionSetParam.js
@@ -11,6 +11,22 @@ define(["lodash", "../select/SelectParam", "../BaseParam"], function(_, SelectPa
 				if(this.max)
 					if(this.value && _.isArray(this.value.keys) && this.value.keys.length > this.max)
 						return this.id + " size may not exceed " + this.max;
+				if(this.disjoint)
+					if(this.value && _.isArray(this.value.keys)){
+						var disjointTarget = values[this.disjoint];
+						if(disjointTarget && _.isArray(disjointTarget.keys)){
+							var intersect = _.intersection(disjointTarget.keys, this.value.keys);
+							if(intersect.length > 0)
+								return this.id + " and " + this.disjoint + " must be disjoint sets but have "
+									+ intersect.length
+									+ " element in common: "
+									+ intersect.slice(0, 9).join(",")
+									+ (intersect.length>10 ? "..." : "");
+
+
+						}
+					}
+
 			};
 		};
 		SelectionSetParam.prototype = new SelectParam();
