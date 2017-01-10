08d9334181f4200331aba098bd318ffa40718a59
Antony Partensky <apartensky@gmail.com>
2017-01-10 11:58:12 -0500

    SelectionSetParam: fix disjoint multiselect validation to ensure no two sets have the same element

--- web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/analysis/src/params/model/selectionSet/SelectionSetParam.js
+++ web/src/main/javascript/edu/dfci/cccb/mev/web/libs/domain/analysis/src/params/model/selectionSet/SelectionSetParam.js
@@ -12,7 +12,7 @@ define(["lodash", "../select/SelectParam", "../BaseParam"], function(_, SelectPa
 					if(this.value && _.isArray(this.value.keys) && this.value.keys.length > this.max)
 						return this.id + " size may not exceed " + this.max;
 				if(this.disjoint){
-					if(_.isString(this.disjoint))
+					if(!this.multiselect)
 						if(this.value && _.isArray(this.value.keys)){
 							var disjointTarget = values[this.disjoint];
 							if(disjointTarget && _.isArray(disjointTarget.keys)){
@@ -30,16 +30,19 @@ define(["lodash", "../select/SelectParam", "../BaseParam"], function(_, SelectPa
 					if(this.multiselect){
 						var value = this.getValue();
 						if(_.isArray(value)){
-							var selectionKeySets = value.map(function(selection){
-								return selection.keys;
-							});
-							var intersect = _.intersection.apply(null, selectionKeySets);
-							if(intersect.length > 0)
-								return "Selections  must be disjoint sets but have "
-									+ intersect.length
-									+ " element in common: "
-									+ intersect.slice(0, 9).join(",")
-									+ (intersect.length>10 ? "..." : "");
+							for(var i=0; i<value.length; i++){
+								for(var j=i+1; j<value.length; j++){
+									var intersect = _.intersection(value[i].keys, value[j].keys);
+									if(intersect.length > 0)
+										return "Selections  must be disjoint sets but "
+											+ value[i].name + " and " + value[j].name
+											+ " have "
+											+ intersect.length
+											+ " element in common: "
+											+ intersect.slice(0, 9).join(",")
+											+ (intersect.length>10 ? "..." : "");
+								}
+							}
 						}
 					}
 				}
