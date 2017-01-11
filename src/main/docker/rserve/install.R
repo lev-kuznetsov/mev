fd14cba8e9c9f60b0ad3c28c83fd8f22b728a5ae
levk <levk@flying-dutchman.dfci.harvard.edu>
2017-01-11 14:12:28 -0500

    Inlined rserve docker definition

--- src/main/docker/rserve/install.R
+++
@@ -1,3 +0,0 @@
-source ('http://bioconductor.org/biocLite.R');
-biocLite (c ('topGO', 'org.Hs.eg.db', 'org.Mm.eg.db', 'limma', 'DESeq', 'edgeR', 'ReactomePA', 'metagenomeSeq', 'DESeq2', 'impute', 'preprocessCore'));
-install.packages (c ('Rserve', 'rafalib', 'amap'));
